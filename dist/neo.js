bedf3bd3b5cfa2b0c8013d2610f669f6c8d20064
qitmac000408 <qitmac000408@qitmac000408deMacBook-Pro.local>
2017-03-20 22:40:23 +0800

    no message

--- dist/neo.js
+++
@@ -1,9 +0,0 @@
-function createElement(type, props) {
-    return {
-        type: type,
-        props: props,
-        children: [].slice.call(arguments, 2)
-    }
-}
-//<div><p></p>xx{111}ppp</div>
-var a = createElement('div', {}, createElement('p', {}, 'xxx', 111, 'ppp'))
