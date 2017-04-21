e55d9a9d568ba8a0d83aa886d29e18e127144214
qitmac000408 <qitmac000408@qitmac000408deMacBook-Pro.local>
2017-04-21 16:01:46 +0800

    update

--- karma.conf.js
+++ karma.conf.js
@@ -9,9 +9,7 @@ module.exports = function(config) {
 
         // base path that will be used to resolve all patterns (eg. files, exclude)
         //  basePath: '',
-        onExit: function(a) {
-            console.log(a)
-        },
+
 
         // frameworks to use
         // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
