eb0809d30f02a8a80746f3b310d22af04d60b1ec
qitmac000408 <qitmac000408@qitmac000408deMacBook-Pro.local>
2017-05-25 12:07:46 +0800

    添加自动化构建

---
+++ s.js
@@ -0,0 +1,23 @@
+var selenium = require('selenium-standalone');
+
+selenium.install({
+  // check for more recent versions of selenium here:
+  // https://selenium-release.storage.googleapis.com/index.html
+  baseURL: 'https://selenium-release.storage.googleapis.com',
+  drivers: {
+    chrome: {
+      // check for more recent versions of chrome driver here:
+      // https://chromedriver.storage.googleapis.com/index.html
+      version: '2.27',
+      arch: process.arch,
+      baseURL: 'https://chromedriver.storage.googleapis.com'
+    }
+  }
+}, function (err) {
+  if (err) {
+    console.warn(err)
+  } else {
+    console.log('安装成功')
+  }
+
+});
