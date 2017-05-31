07db912fc49e9487ef117ace9fcf191d00737aaf
qitmac000408 <qitmac000408@qitmac000408deMacBook-Pro.local>
2017-05-31 15:13:15 +0800

    add firefox

---
+++ test/spec2.js
@@ -0,0 +1,9 @@
+
+
+import './modules/createElement.spec'
+import './modules/util.spec'
+import './modules/style.spec'
+
+import './modules/classInherit.spec'
+import './modules/shallow.spec'
+import './modules/browser.spec'
