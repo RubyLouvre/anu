c00ec6f847b498e1ad97db037440a5ddefb72079
qitmac000408 <qitmac000408@qitmac000408deMacBook-Pro.local>
2017-06-06 14:46:25 +0800

    delete history

--- .history/test/spec2_20170531113039.js
+++
@@ -1,9 +0,0 @@
-
-
-import './modules/createElement.spec'
-import './modules/util.spec'
-import './modules/style.spec'
-
-import './modules/classInherit.spec'
-import './modules/shallow.spec'
-import './modules/browser.spec'
