3a24239fba18595e94775c28e9277e1ed4ffbeb4
rubylouvre <cheng19840218@gmail.com>
2017-06-11 20:47:22 +0800

    添加新的东西

---
+++ src/index9.js
@@ -0,0 +1,25 @@
+class Select extends React.Component{
+     constructor() {
+        super()
+        this.state = {
+            value: 'bbb'
+        }
+        this.onChange = this.onChange.bind(this)
+    }
+    onChange(e){
+       console.log(e.target.value)
+       this.setState({
+           value: e.target.value
+       })
+    }
+    render() {
+        return <select multiple="true" value={this.state.value} onChange={this.onChange}>
+            <option value='aaa'>aaa</option>
+            <option value='bbb'>bbb</option>
+            <option value='ccc'>ccc</option>
+        </select>
+    }
+}
+window.onload = function(){
+    window.s = ReactDOM.render( <Select />, document.getElementById('example'))
+}
