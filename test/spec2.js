51c72c5ce06eec4cea799a7dcdf79836d6c968dd
qiang.guang <qiang.guang@qunar.com>
2017-06-15 15:35:40 +0800

    修改配置文件冲突

--- test/spec2.js
+++ test/spec2.js
@@ -6,7 +6,3 @@ import './modules/classInherit.spec'
 import './modules/shallow.spec'
 import './modules/browser.spec'
 import './modules/cloneElement.spec'
-<<<<<<< HEAD
-=======
-
->>>>>>> 2a9168aa5d64644ba7364e8688f636544a4d6029
