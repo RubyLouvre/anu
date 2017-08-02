bf3e70d78824476a747e3c0106b61bc2c3912893
qitmac000408 <qitmac000408@qitmac000408deMacBook-Pro.local>
2017-08-02 09:33:56 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
         "node": true
     },
     "extends": "eslint:recommended",
+    "parser": "babel-eslint",
     "parserOptions": {
         "ecmaFeatures": {
             "experimentalObjectRestSpread": true,
@@ -14,34 +15,58 @@ module.exports = {
         },
         "sourceType": "module"
     },
-    "globals": {
-        "module": true,
-        "define": true,
-        "exports": true
-    },
     "plugins": [
         "react"
     ],
+    "globals": {
+        "define": true,
+        "expect": true,
+        "describe": true,
+        "it": true,
+        "webpackIsomorphicTools": true,
+        "__CLIENT__": true,
+        "__SERVER__": true,
+        "__DEVELOPMENT__": true,
+        "__DEVTOOLS__": true,
+        "__INITIAL_STATE__": true
+    },
     "rules": {
-        "indent": [
-            "error",
-            4
-        ],
         "strict": 0,
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
+        "indent": ["error", 2],
+        "linebreak-style": ["error", "unix"],
+        "quotes": ["error", "double"],
+        "semi": ["error", "always"],
+        "comma-dangle": [2, "only-multiline"],
+        "eqeqeq": ["error", "always"],
+        "no-alert": "error",
+        "no-eval": "error",
+        "guard-for-in": "error",
+        "no-new-func": "error",
+        "no-with": "error",
+        "for-direction": "error",
         "curly": "error",
         "brace-style": ["error", "1tbs"],
-        "semi": [
-            "error",
-            "always"
-        ]
+        "no-bitwise": "error",
+        "no-extend-native": "error",
+        "array-callback-return": "error",
+        "no-useless-call": "error",
+        "no-sequences": "error",
+        "no-restricted-properties": [2, {
+            "property": "localStorage"
+        }],
+        "no-unmodified-loop-condition": "error",
+        "no-underscore-dangle": ["off"],
+        "react/prefer-stateless-function": ["off"],
+        "max-len": ["error", 200, 4],
+        "one-var": ["off"],
+        "no-confusing-arrow": "off",
+        "react/jsx-no-bind": ["off"],
+        "react/jsx-indent": [2, 4],
+        "react/jsx-indent-props": [2, 4],
+        "react/jsx-boolean-value": ["off"],
+        "eol-last": 0,
+        "no-param-reassign": [1],
+        "no-template-curly-in-string": "error",
+        "radix": "error"
     }
 };
-
