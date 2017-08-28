464c6795260e88755ffab170b0e7e7bfc67906d6
rubylouvre <cheng19840218@gmail.com>
2017-08-29 00:25:38 +0800

    简化rax

--- dist/ReactIE.js
+++ dist/ReactIE.js
@@ -917,203 +917,112 @@ function setStateImpl(state, cb) {
     }
 }
 
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
 /**
  * 为了兼容0.13之前的版本
  */
-var MANY = "DEFINE_MANY";
-var MANY_MERGED = "MANY_MERGED";
-var ReactClassInterface = {
-  mixins: MANY,
-  statics: MANY,
-  propTypes: MANY,
-  contextTypes: MANY,
-  childContextTypes: MANY,
-  getDefaultProps: MANY_MERGED,
-  getInitialState: MANY_MERGED,
-  getChildContext: MANY_MERGED,
-  render: "ONCE",
-  componentWillMount: MANY,
-  componentDidMount: MANY,
-  componentWillReceiveProps: MANY,
-  shouldComponentUpdate: "DEFINE_ONCE",
-  componentWillUpdate: MANY,
-  componentDidUpdate: MANY,
-  componentWillUnmount: MANY
+var AUTOBIND_BLACKLIST = {
+    render: 1,
+    shouldComponentUpdate: 1,
+    componentWillReceiveProps: 1,
+    componentWillUpdate: 1,
+    componentDidUpdate: 1,
+    componentWillMount: 1,
+    componentDidMount: 1,
+    componentWillUnmount: 1,
+    componentDidUnmount: 1
 };
 
-var specHandle = {
-  displayName: function displayName(Ctor, value, name) {
-    Ctor[name] = value;
-  },
-  mixins: function mixins(Ctor, value) {
-    if (value) {
-      for (var i = 0; i < value.length; i++) {
-        mixSpecIntoComponent(Ctor, value[i]);
-      }
-    }
-  },
+function collectMixins(mixins) {
+    var keyed = {};
 
-  propTypes: mergeObject,
-  childContextTypes: mergeObject,
-  contextTypes: mergeObject,
-
-  getDefaultProps: function getDefaultProps(Ctor, value, name) {
-    if (Ctor[name]) {
-      Ctor[name] = createMergedResultFunction(Ctor[name], value);
-    } else {
-      Ctor[name] = value;
-    }
-  },
-  statics: function statics(Ctor, value) {
-    extend(Ctor, Object(value));
-  },
-
-  autobind: noop
-};
-
-function mergeObject(fn, value, name) {
-  fn[name] = Object.assign({}, fn[name], value);
-}
-
-//防止覆盖Component内部一些重要的方法或属性
-var protectedProps = {
-  mixin: 1,
-  setState: 1,
-  forceUpdate: 1,
-  __processPendingState: 1,
-  __pendingCallbacks: 1,
-  __pendingStates: 1
-};
-
-function mixSpecIntoComponent(Ctor, spec) {
-  if (!spec) {
-    return;
-  }
-  if (isFn(spec)) {
-    console.warn("createClass(spec)中的spec不能为函数，只能是纯对象"); // eslint-disable-line
-  }
-
-  var proto = Ctor.prototype;
-  var autoBindPairs = proto.__reactAutoBindPairs;
-
-  if (spec.hasOwnProperty("mixin")) {
-    specHandle.mixins(Ctor, spec.mixins);
-  }
-
-  for (var name in spec) {
-    if (!spec.hasOwnProperty(name)) {
-      continue;
-    }
-    if (protectedProps[name] === 1) {
-      continue;
-    }
-
-    var property = spec[name];
-    var isAlreadyDefined = proto.hasOwnProperty(name);
-
-    if (specHandle.hasOwnProperty(name)) {
-      specHandle[name](Ctor, property, name);
-    } else {
-      var isReactClassMethod = ReactClassInterface.hasOwnProperty(name);
-      var shouldAutoBind = isFn(property) && !isReactClassMethod && !isAlreadyDefined && spec.autobind !== false;
-
-      if (shouldAutoBind) {
-        autoBindPairs.push(name, property);
-        proto[name] = property;
-      } else {
-        if (isAlreadyDefined) {
-          var specPolicy = ReactClassInterface[name];
-          //合并多个同名函数
-          if (specPolicy === MANY_MERGED) {
-            //这个是有返回值
-            proto[name] = createMergedResultFunction(proto[name], property);
-          } else if (specPolicy === MANY) {
-            //这个没有返回值
-            proto[name] = createChainedFunction(proto[name], property);
-          }
-        } else {
-          proto[name] = property;
+    for (var i = 0; i < mixins.length; i++) {
+        var mixin = mixins[i];
+        if (mixin.mixins) {
+            applyMixins(mixin, collectMixins(mixin.mixins));
         }
-      }
+
+        for (var key in mixin) {
+            if (mixin.hasOwnProperty(key) && key !== 'mixins') {
+                (keyed[key] || (keyed[key] = [])).push(mixin[key]);
+            }
+        }
     }
-  }
-}
 
-function mergeOwnProperties(one, two) {
-  for (var key in two) {
-    if (two.hasOwnProperty(key)) {
-      one[key] = two[key];
+    return keyed;
+}
+var MANY_MERGED = {
+    getInitialState: 1,
+    getDefaultProps: 1,
+    getChildContext: 1
+};
+function flattenHooks(key, hooks) {
+    var hookType = _typeof(hooks[0]);
+    if (hookType === 'object') {
+        // Merge objects
+        hooks.unshift({});
+        return Object.assign.apply(null, hooks);
+    } else if (hookType === 'function') {
+        return function () {
+            var ret = void 0;
+            for (var i = 0; i < hooks.length; i++) {
+                var r = hooks[i].apply(this, arguments);
+                if (r && MANY_MERGED[key]) {
+                    if (!ret) ret = {};
+                    Object.assign(ret, r);
+                }
+            }
+            return ret;
+        };
+    } else {
+        return hooks[0];
     }
-  }
-  return one;
 }
 
-function createMergedResultFunction(one, two) {
-  return function mergedResult() {
-    var a = one.apply(this, arguments);
-    var b = two.apply(this, arguments);
-    if (a == null) {
-      return b;
-    } else if (b == null) {
-      return a;
+function applyMixins(proto, mixins) {
+    for (var key in mixins) {
+        if (mixins.hasOwnProperty(key)) {
+            proto[key] = flattenHooks(key, mixins[key].concat(proto[key] || []));
+        }
     }
-    var c = {};
-    mergeOwnProperties(c, a);
-    mergeOwnProperties(c, b);
-    return c;
-  };
-}
-
-function createChainedFunction(one, two) {
-  return function chainedFunction() {
-    one.apply(this, arguments);
-    two.apply(this, arguments);
-  };
-}
-
-function bindAutoBindMethod(component, method) {
-  var boundMethod = method.bind(component);
-  return boundMethod;
-}
-
-function bindAutoBindMethods(component) {
-  var pairs = component.__reactAutoBindPairs;
-  for (var i = 0; i < pairs.length; i += 2) {
-    var autoBindKey = pairs[i];
-    var method = pairs[i + 1];
-    component[autoBindKey] = bindAutoBindMethod(component, method);
-  }
 }
 
 //创建一个构造器
 function newCtor(className) {
-  var curry = Function("ReactComponent", "bindAutoBindMethods", "return function " + className + "(props, context) {\n    ReactComponent.call(this, props, context);\n    this.state = this.getInitialState ? this.getInitialState() : {};\n    if (this.__reactAutoBindPairs.length) {\n      bindAutoBindMethods(this);\n    }\n  };");
-  return curry(Component, bindAutoBindMethods);
+    var curry = Function("ReactComponent", "list", "return function " + className + "(props, context) {\n      ReactComponent.call(this, props, context);\n\n     for (let methodName in this) {\n        let method = this[methodName];\n        if (typeof method  === 'function'&& !list[methodName]) {\n          this[methodName] = method.bind(this);\n        }\n      }\n\n      if (spec.getInitialState) {\n        this.state = spec.getInitialState.call(this);\n      }\n\n  };");
+    return curry(Component, AUTOBIND_BLACKLIST);
 }
+
 var warnOnce = 1;
 function createClass(spec) {
-  if (warnOnce) {
-    warnOnce = 0;
-    console.warn("createClass已经过时，强烈建议使用es6方式定义类"); // eslint-disable-line
-  }
-  var Constructor = newCtor(spec.displayName || "Component");
-  var proto = inherit(Constructor, Component);
-  proto.__reactAutoBindPairs = [];
-  delete proto.render;
-
-  mixSpecIntoComponent(Constructor, spec);
-  if (isFn(Constructor.getDefaultProps)) {
-    Constructor.defaultProps = Constructor.getDefaultProps();
-  }
-
-  //性能优化，为了防止在原型链进行无用的查找，直接将用户没有定义的生命周期钩子置为null
-  for (var methodName in ReactClassInterface) {
-    if (!proto[methodName]) {
-      proto[methodName] = null;
+    if (warnOnce) {
+        warnOnce = 0;
+        console.warn("createClass已经过时，强烈建议使用es6方式定义类"); // eslint-disable-line
     }
-  }
+    var Constructor = newCtor(spec.displayName || "Component");
+    var proto = inherit(Constructor, Component);
+    //如果mixins里面非常复杂，可能mixin还包含其他mixin
+    if (spec.mixins) {
+        applyMixins(spec, collectMixins(spec.mixins));
+    }
 
-  return Constructor;
+    extend(proto, spec);
+
+    if (spec.statics) {
+        extend(Constructor, spec.statics);
+    }
+    "propTypes,contextTypes,childContextTypes,displayName".replace(/\w+/g, function (name) {
+        if (spec[name]) {
+            Constructor[name] = spec[name];
+        }
+    });
+
+    if (isFn(spec.getDefaultProps)) {
+        Constructor.defaultProps = spec.getDefaultProps();
+    }
+
+    return Constructor;
 }
 
 function cloneElement(vnode, props) {
--- src/createClass.js
+++ src/createClass.js
@@ -1,213 +1,126 @@
-import { extend, isFn, inherit, noop } from "./util";
-import { Component } from "./Component";
+import {extend, isFn, inherit, typeNumber} from "./util";
+import {Component} from "./Component";
 /**
  * 为了兼容0.13之前的版本
  */
-var MANY = "DEFINE_MANY";
-var MANY_MERGED = "MANY_MERGED";
-var ReactClassInterface = {
-  mixins: MANY,
-  statics: MANY,
-  propTypes: MANY,
-  contextTypes: MANY,
-  childContextTypes: MANY,
-  getDefaultProps: MANY_MERGED,
-  getInitialState: MANY_MERGED,
-  getChildContext: MANY_MERGED,
-  render: "ONCE",
-  componentWillMount: MANY,
-  componentDidMount: MANY,
-  componentWillReceiveProps: MANY,
-  shouldComponentUpdate: "DEFINE_ONCE",
-  componentWillUpdate: MANY,
-  componentDidUpdate: MANY,
-  componentWillUnmount: MANY
+const AUTOBIND_BLACKLIST = {
+    render: 1,
+    shouldComponentUpdate: 1,
+    componentWillReceiveProps: 1,
+    componentWillUpdate: 1,
+    componentDidUpdate: 1,
+    componentWillMount: 1,
+    componentDidMount: 1,
+    componentWillUnmount: 1,
+    componentDidUnmount: 1
 };
 
-var specHandle = {
-  displayName(Ctor, value, name) {
-    Ctor[name] = value;
-  },
-  mixins(Ctor, value) {
-    if (value) {
-      for (var i = 0; i < value.length; i++) {
-        mixSpecIntoComponent(Ctor, value[i]);
-      }
-    }
-  },
-  propTypes: mergeObject,
-  childContextTypes: mergeObject,
-  contextTypes: mergeObject,
+function collectMixins(mixins) {
+    let keyed = {};
 
-  getDefaultProps(Ctor, value, name) {
-    if (Ctor[name]) {
-      Ctor[name] = createMergedResultFunction(Ctor[name], value);
-    } else {
-      Ctor[name] = value;
-    }
-  },
-
-  statics(Ctor, value) {
-    extend(Ctor, Object(value));
-  },
-  autobind: noop
-};
-
-function mergeObject(fn, value, name) {
-  fn[name] = Object.assign({}, fn[name], value);
-}
-
-//防止覆盖Component内部一些重要的方法或属性
-var protectedProps = {
-  mixin: 1,
-  setState: 1,
-  forceUpdate: 1,
-  __processPendingState: 1,
-  __pendingCallbacks: 1,
-  __pendingStates: 1
-};
-
-function mixSpecIntoComponent(Ctor, spec) {
-  if (!spec) {
-    return;
-  }
-  if (isFn(spec)) {
-    console.warn("createClass(spec)中的spec不能为函数，只能是纯对象"); // eslint-disable-line
-  }
-
-  var proto = Ctor.prototype;
-  var autoBindPairs = proto.__reactAutoBindPairs;
-
-  if (spec.hasOwnProperty("mixin")) {
-    specHandle.mixins(Ctor, spec.mixins);
-  }
-
-  for (var name in spec) {
-    if (!spec.hasOwnProperty(name)) {
-      continue;
-    }
-    if (protectedProps[name] === 1) {
-      continue;
-    }
-
-    var property = spec[name];
-    var isAlreadyDefined = proto.hasOwnProperty(name);
-
-    if (specHandle.hasOwnProperty(name)) {
-      specHandle[name](Ctor, property, name);
-    } else {
-      var isReactClassMethod = ReactClassInterface.hasOwnProperty(name);
-      var shouldAutoBind =
-        isFn(property) &&
-        !isReactClassMethod &&
-        !isAlreadyDefined &&
-        spec.autobind !== false;
-
-      if (shouldAutoBind) {
-        autoBindPairs.push(name, property);
-        proto[name] = property;
-      } else {
-        if (isAlreadyDefined) {
-          var specPolicy = ReactClassInterface[name];
-          //合并多个同名函数
-          if (specPolicy === MANY_MERGED) {
-            //这个是有返回值
-            proto[name] = createMergedResultFunction(proto[name], property);
-          } else if (specPolicy === MANY) {
-            //这个没有返回值
-            proto[name] = createChainedFunction(proto[name], property);
-          }
-        } else {
-          proto[name] = property;
+    for (let i = 0; i < mixins.length; i++) {
+        let mixin = mixins[i];
+        if (mixin.mixins) {
+            applyMixins(mixin, collectMixins(mixin.mixins));
         }
-      }
+
+        for (let key in mixin) {
+            if (mixin.hasOwnProperty(key) && key !== 'mixins') {
+                (keyed[key] || (keyed[key] = [])).push(mixin[key]);
+            }
+        }
     }
-  }
-}
 
-function mergeOwnProperties(one, two) {
-  for (var key in two) {
-    if (two.hasOwnProperty(key)) {
-      one[key] = two[key];
+    return keyed;
+}
+var MANY_MERGED = {
+    getInitialState: 1,
+    getDefaultProps: 1,
+    getChildContext: 1
+}
+function flattenHooks(key, hooks) {
+    let hookType = typeof hooks[0];
+    if (hookType === 'object') {
+        // Merge objects
+        hooks.unshift({});
+        return Object
+            .assign
+            .apply(null, hooks);
+    } else if (hookType === 'function') {
+        return function () {
+            let ret;
+            for (let i = 0; i < hooks.length; i++) {
+                let r = hooks[i].apply(this, arguments);
+                if (r && MANY_MERGED[key]) {
+                    if (!ret) 
+                        ret = {};
+                    Object.assign(ret, r);
+                }
+            }
+            return ret;
+        };
+    } else {
+        return hooks[0];
     }
-  }
-  return one;
 }
 
-function createMergedResultFunction(one, two) {
-  return function mergedResult() {
-    var a = one.apply(this, arguments);
-    var b = two.apply(this, arguments);
-    if (a == null) {
-      return b;
-    } else if (b == null) {
-      return a;
+function applyMixins(proto, mixins) {
+    for (let key in mixins) {
+        if (mixins.hasOwnProperty(key)) {
+            proto[key] = flattenHooks(key, mixins[key].concat(proto[key] || []));
+        }
     }
-    var c = {};
-    mergeOwnProperties(c, a);
-    mergeOwnProperties(c, b);
-    return c;
-  };
-}
-
-function createChainedFunction(one, two) {
-  return function chainedFunction() {
-    one.apply(this, arguments);
-    two.apply(this, arguments);
-  };
-}
-
-function bindAutoBindMethod(component, method) {
-  var boundMethod = method.bind(component);
-  return boundMethod;
-}
-
-function bindAutoBindMethods(component) {
-  var pairs = component.__reactAutoBindPairs;
-  for (var i = 0; i < pairs.length; i += 2) {
-    var autoBindKey = pairs[i];
-    var method = pairs[i + 1];
-    component[autoBindKey] = bindAutoBindMethod(component, method);
-  }
 }
 
 //创建一个构造器
 function newCtor(className) {
-  var curry = Function(
-    "ReactComponent",
-    "bindAutoBindMethods",
+    var curry = Function("ReactComponent", "list", 
     `return function ${className}(props, context) {
-    ReactComponent.call(this, props, context);
-    this.state = this.getInitialState ? this.getInitialState() : {};
-    if (this.__reactAutoBindPairs.length) {
-      bindAutoBindMethods(this);
-    }
-  };`
-  );
-  return curry(Component, bindAutoBindMethods);
+      ReactComponent.call(this, props, context);
+
+     for (let methodName in this) {
+        let method = this[methodName];
+        if (typeof method  === 'function'&& !list[methodName]) {
+          this[methodName] = method.bind(this);
+        }
+      }
+
+      if (spec.getInitialState) {
+        this.state = spec.getInitialState.call(this);
+      }
+
+  };`);
+    return curry(Component, AUTOBIND_BLACKLIST);
 }
+
 var warnOnce = 1;
 export function createClass(spec) {
-  if (warnOnce) {
-    warnOnce = 0;
-    console.warn("createClass已经过时，强烈建议使用es6方式定义类"); // eslint-disable-line
-  }
-  var Constructor = newCtor(spec.displayName || "Component");
-  var proto = inherit(Constructor, Component);
-  proto.__reactAutoBindPairs = [];
-  delete proto.render;
-
-  mixSpecIntoComponent(Constructor, spec);
-  if (isFn(Constructor.getDefaultProps)) {
-    Constructor.defaultProps = Constructor.getDefaultProps();
-  }
-
-  //性能优化，为了防止在原型链进行无用的查找，直接将用户没有定义的生命周期钩子置为null
-  for (var methodName in ReactClassInterface) {
-    if (!proto[methodName]) {
-      proto[methodName] = null;
+    if (warnOnce) {
+        warnOnce = 0;
+        console.warn("createClass已经过时，强烈建议使用es6方式定义类"); // eslint-disable-line
     }
-  }
+    var Constructor = newCtor(spec.displayName || "Component");
+    var proto = inherit(Constructor, Component);
+    //如果mixins里面非常复杂，可能mixin还包含其他mixin
+    if (spec.mixins) {
+        applyMixins(spec, collectMixins(spec.mixins));
+    }
 
-  return Constructor;
+    extend(proto, spec);
+
+    if (spec.statics) {
+       extend(Constructor, spec.statics);
+    }
+    "propTypes,contextTypes,childContextTypes,displayName"
+        .replace(/\w+/g, function (name) {
+            if (spec[name]) {
+                Constructor[name] = spec[name];
+            }
+        })
+
+    if (isFn(spec.getDefaultProps)) {
+        Constructor.defaultProps = spec.getDefaultProps();
+    }
+
+    return Constructor;
 }
