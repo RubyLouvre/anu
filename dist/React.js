48e69c2fa877aab634e21312317db3631700f85c
RubyLouvre <1669866773@qq.com>
2017-09-10 13:13:09 +0800

    简化namespace

--- dist/React.js
+++ dist/React.js
@@ -417,9 +417,7 @@ function DOMElement(type) {
 var NAMESPACE = {
     svg: "http://www.w3.org/2000/svg",
     xmlns: "http://www.w3.org/2000/xmlns/",
-    xml: "http://www.w3.org/XML/1998/namespace",
     xlink: "http://www.w3.org/1999/xlink",
-    xhtml: "http://www.w3.org/1999/xhtml",
     math: "http://www.w3.org/1998/Math/MathML"
 };
 
