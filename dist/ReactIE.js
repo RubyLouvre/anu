9f3ae707b1437e877eb0a7474f0aee85e2d9b441
RubyLouvre <1669866773@qq.com>
2017-09-10 11:11:06 +0800

    make codeclimate happy!

--- dist/ReactIE.js
+++ dist/ReactIE.js
@@ -1492,106 +1492,106 @@ var propAdapters = {
  反之，它就是非受控组件，非受控组件会在框架内部添加一些事件，阻止**状态属性**被用户的行为改变，只能被setState改变
  */
 function processFormElement(vnode, dom, props) {
-  var domType = dom.type;
-  var duplexType = duplexMap[domType];
-  if (duplexType) {
-    var data = duplexData[duplexType];
-    var duplexProp = data[0];
-    var keys = data[1];
-    var eventName = data[2];
-    if (duplexProp in props && !hasOtherControllProperty(props, keys)) {
-      // eslint-disable-next-line
-      console.warn("\u4F60\u4E3A" + vnode.type + "[type=" + domType + "]\u5143\u7D20\u6307\u5B9A\u4E86" + duplexProp + "\u5C5E\u6027\uFF0C\n      \u4F46\u662F\u6CA1\u6709\u63D0\u4F9B\u53E6\u5916\u7684" + Object.keys(keys) + "\u6765\u63A7\u5236" + duplexProp + "\u5C5E\u6027\u7684\u53D8\u5316\n      \u90A3\u4E48\u5B83\u5373\u4E3A\u4E00\u4E2A\u975E\u53D7\u63A7\u7EC4\u4EF6\uFF0C\u7528\u6237\u65E0\u6CD5\u901A\u8FC7\u8F93\u5165\u6539\u53D8\u5143\u7D20\u7684" + duplexProp + "\u503C");
-      dom[eventName] = data[3];
+    var domType = dom.type;
+    var duplexType = duplexMap[domType];
+    if (duplexType) {
+        var data = duplexData[duplexType];
+        var duplexProp = data[0];
+        var keys = data[1];
+        var eventName = data[2];
+        if (duplexProp in props && !hasOtherControllProperty(props, keys)) {
+            // eslint-disable-next-line
+            console.warn("\u4F60\u4E3A" + vnode.type + "[type=" + domType + "]\u5143\u7D20\u6307\u5B9A\u4E86" + duplexProp + "\u5C5E\u6027\uFF0C\n      \u4F46\u662F\u6CA1\u6709\u63D0\u4F9B\u53E6\u5916\u7684" + Object.keys(keys) + "\u6765\u63A7\u5236" + duplexProp + "\u5C5E\u6027\u7684\u53D8\u5316\n      \u90A3\u4E48\u5B83\u5373\u4E3A\u4E00\u4E2A\u975E\u53D7\u63A7\u7EC4\u4EF6\uFF0C\u7528\u6237\u65E0\u6CD5\u901A\u8FC7\u8F93\u5165\u6539\u53D8\u5143\u7D20\u7684" + duplexProp + "\u503C");
+            dom[eventName] = data[3];
+        }
+        if (duplexType === 3) {
+            postUpdateSelectedOptions(vnode);
+        }
     }
-    if (duplexType === 3) {
-      postUpdateSelectedOptions(vnode);
-    }
-  }
 }
 
 function hasOtherControllProperty(props, keys) {
-  for (var key in props) {
-    if (keys[key]) {
-      return true;
+    for (var key in props) {
+        if (keys[key]) {
+            return true;
+        }
     }
-  }
 }
 var duplexMap = {
-  color: 1,
-  date: 1,
-  datetime: 1,
-  "datetime-local": 1,
-  email: 1,
-  month: 1,
-  number: 1,
-  password: 1,
-  range: 1,
-  search: 1,
-  tel: 1,
-  text: 1,
-  time: 1,
-  url: 1,
-  week: 1,
-  textarea: 1,
-  checkbox: 2,
-  radio: 2,
-  "select-one": 3,
-  "select-multiple": 3
+    color: 1,
+    date: 1,
+    datetime: 1,
+    "datetime-local": 1,
+    email: 1,
+    month: 1,
+    number: 1,
+    password: 1,
+    range: 1,
+    search: 1,
+    tel: 1,
+    text: 1,
+    time: 1,
+    url: 1,
+    week: 1,
+    textarea: 1,
+    checkbox: 2,
+    radio: 2,
+    "select-one": 3,
+    "select-multiple": 3
 };
 
 function preventUserInput(e) {
-  var target = e.target;
-  var name = e.type === "textarea" ? "innerHTML" : "value";
-  target[name] = target._lastValue;
+    var target = e.target;
+    var name = e.type === "textarea" ? "innerHTML" : "value";
+    target[name] = target._lastValue;
 }
 
 function preventUserClick(e) {
-  e.preventDefault();
+    e.preventDefault();
 }
 
 function preventUserChange(e) {
-  var target = e.target;
-  var value = target._lastValue;
-  var options$$1 = target.options;
-  if (target.multiple) {
+    var target = e.target;
+    var value = target._lastValue;
+    var options$$1 = target.options;
+    if (target.multiple) {
 
-    updateOptionsMore(options$$1, options$$1.length, value);
-  } else {
-    updateOptionsOne(options$$1, options$$1.length, value);
-  }
+        updateOptionsMore(options$$1, options$$1.length, value);
+    } else {
+        updateOptionsOne(options$$1, options$$1.length, value);
+    }
 }
 
 var duplexData = {
-  1: ["value", {
-    onChange: 1,
-    onInput: 1,
-    readOnly: 1,
-    disabled: 1
-  }, "oninput", preventUserInput],
-  2: ["checked", {
-    onChange: 1,
-    onClick: 1,
-    readOnly: 1,
-    disabled: 1
-  }, "onclick", preventUserClick],
-  3: ["value", {
-    onChange: 1,
-    disabled: 1
-  }, "onchange", preventUserChange]
+    1: ["value", {
+        onChange: 1,
+        onInput: 1,
+        readOnly: 1,
+        disabled: 1
+    }, "oninput", preventUserInput],
+    2: ["checked", {
+        onChange: 1,
+        onClick: 1,
+        readOnly: 1,
+        disabled: 1
+    }, "onclick", preventUserClick],
+    3: ["value", {
+        onChange: 1,
+        disabled: 1
+    }, "onchange", preventUserChange]
 };
 
 function postUpdateSelectedOptions(vnode) {
-  var props = vnode.props,
-      multiple = !!props.multiple,
-      value = typeNumber(props.value) > 1 ? props.value : typeNumber(props.defaultValue) > 1 ? props.defaultValue : multiple ? [] : "",
-      options$$1 = [];
-  collectOptions(vnode, props, options$$1);
-  if (multiple) {
-    updateOptionsMore(options$$1, options$$1.length, value);
-  } else {
-    updateOptionsOne(options$$1, options$$1.length, value);
-  }
+    var props = vnode.props,
+        multiple = !!props.multiple,
+        value = typeNumber(props.value) > 1 ? props.value : typeNumber(props.defaultValue) > 1 ? props.defaultValue : multiple ? [] : "",
+        options$$1 = [];
+    collectOptions(vnode, props, options$$1);
+    if (multiple) {
+        updateOptionsMore(options$$1, options$$1.length, value);
+    } else {
+        updateOptionsOne(options$$1, options$$1.length, value);
+    }
 }
 
 /**
@@ -1602,72 +1602,72 @@ function postUpdateSelectedOptions(vnode) {
  * @param {Array} ret
  */
 function collectOptions(vnode, props, ret) {
-  var arr = props.children;
-  for (var i = 0, n = arr.length; i < n; i++) {
-    var el = arr[i];
-    if (el.type === "option") {
-      ret.push(el);
-    } else if (el.type === "optgroup") {
-      collectOptions(el, el.props, ret);
+    var arr = props.children;
+    for (var i = 0, n = arr.length; i < n; i++) {
+        var el = arr[i];
+        if (el.type === "option") {
+            ret.push(el);
+        } else if (el.type === "optgroup") {
+            collectOptions(el, el.props, ret);
+        }
     }
-  }
 }
 
 function updateOptionsOne(options$$1, n, propValue) {
-  var selectedValue = "" + propValue;
-  for (var i = 0; i < n; i++) {
-    var option = options$$1[i];
-    var value = getOptionValue(option, option.props);
-    if (value === selectedValue) {
-      getOptionSelected(option, true);
-      return;
+    var selectedValue = "" + propValue;
+    for (var i = 0; i < n; i++) {
+        var option = options$$1[i];
+        var value = getOptionValue(option, option.props);
+        if (value === selectedValue) {
+            getOptionSelected(option, true);
+            return;
+        }
     }
-  }
-  if (n) {
-    getOptionSelected(options$$1[0], true);
-  }
+    if (n) {
+        getOptionSelected(options$$1[0], true);
+    }
 }
 
 function updateOptionsMore(options$$1, n, propValue) {
-  var selectedValue = {};
-  try {
-    for (var i = 0; i < propValue.length; i++) {
-      selectedValue["&" + propValue[i]] = true;
+    var selectedValue = {};
+    try {
+        for (var i = 0; i < propValue.length; i++) {
+            selectedValue["&" + propValue[i]] = true;
+        }
+    } catch (e) {
+        /* istanbul ignore next */
+        console.warn('<select multiple="true"> 的value应该对应一个字符串数组'); // eslint-disable-line
     }
-  } catch (e) {
-    /* istanbul ignore next */
-    console.warn('<select multiple="true"> 的value应该对应一个字符串数组'); // eslint-disable-line
-  }
-  for (var _i = 0; _i < n; _i++) {
-    var option = options$$1[_i];
-    var value = getOptionValue(option, option.props);
-    var selected = selectedValue.hasOwnProperty("&" + value);
-    getOptionSelected(option, selected);
-  }
+    for (var _i = 0; _i < n; _i++) {
+        var option = options$$1[_i];
+        var value = getOptionValue(option, option.props);
+        var selected = selectedValue.hasOwnProperty("&" + value);
+        getOptionSelected(option, selected);
+    }
 }
 
 function getOptionValue(option, props) {
-  if (!props) {
-    return getDOMOptionValue(option);
-  }
-  //这里在1.1.1改动过， props.value === undefined ? props.children[0].text : props.value;
-  return props.value === undefined ? props.children : props.value;
+    if (!props) {
+        return getDOMOptionValue(option);
+    }
+    //这里在1.1.1改动过， props.value === undefined ? props.children[0].text : props.value;
+    return props.value === undefined ? props.children : props.value;
 }
 
 function getDOMOptionValue(node) {
-  if (node.hasAttribute && node.hasAttribute("value")) {
-    return node.getAttribute("value");
-  }
-  var attr = node.getAttributeNode("value");
-  if (attr && attr.specified) {
-    return attr.value;
-  }
-  return node.innerHTML.trim();
+    if (node.hasAttribute && node.hasAttribute("value")) {
+        return node.getAttribute("value");
+    }
+    var attr = node.getAttributeNode("value");
+    if (attr && attr.specified) {
+        return attr.value;
+    }
+    return node.innerHTML.trim();
 }
 
 function getOptionSelected(option, selected) {
-  var dom = option._hostNode || option;
-  dom.selected = selected;
+    var dom = option._hostNode || option;
+    dom.selected = selected;
 }
 
 function disposeVnode(vnode) {
