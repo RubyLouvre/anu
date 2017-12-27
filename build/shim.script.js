29db731f0f4cc6e96009dd829ea429d0acfdf279
RubyLouvre <1669866773@qq.com>
2017-12-27 15:58:24 +0800

    更新build

--- build/shim.script.js
+++ build/shim.script.js
@@ -23,7 +23,7 @@ fs.writeFileSync(dir2, text2, { encoding: "utf8" });
 // fs.writeFileSync( path.join(__dirname, "../../antd-test/other/React.js"), text2, { encoding: "utf8" });
 
 //fs.writeFileSync( path.join(__dirname, "../../yo-demo/node_modules/anujs/dist/React.js"), text2, { encoding: "utf8" });
-fs.writeFileSync( path.join(__dirname, "../../ani/node_modules/anujs/dist/React.js"), text2, { encoding: "utf8" });
+//fs.writeFileSync( path.join(__dirname, "../../ani/node_modules/anujs/dist/React.js"), text2, { encoding: "utf8" });
 
 console.log("对React瘦身完毕"); // eslint-disable-line
 
