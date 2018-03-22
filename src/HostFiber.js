41bbdd74181df9be508c7296a9a215e7e67c401c
RubyLouvre <1669866773@qq.com>
2018-03-22 17:25:38 +0800

    简化其他将要移除的模块

--- src/HostFiber.js
+++ src/HostFiber.js
@@ -1,9 +1,5 @@
 import { Refs } from "./Refs";
-import { formElements, inputControll } from "./inputControll";
-import { returnFalse, returnTrue, emptyObject, extend } from "../src/util";
-import { diffProps } from "./diffProps";
-import { createElement, insertElement } from "./browser";
-
+import {  extend } from "../src/util";
 /**
  * 将虚拟DOM转换为Fiber
  * @param {vnode} vnode 
@@ -19,45 +15,3 @@ export function HostFiber(vnode, parentFiber) {
     this._mountOrder = Refs.mountOrder++;
 }
 
-HostFiber.prototype = {
-    addState: function (state) {
-        let states = this._states;
-        if (states[states.length - 1] !== state) {
-            states.push(state);
-        }
-    },
-    transition(updateQueue) {
-        let state = this._states.shift();
-        if (state) {
-            this[state](updateQueue);
-        }
-    },
-    init(updateQueue, mountCarrier, initChildren) {
-        let dom = (this.stateNode = createElement(this, this.return));
-        let beforeDOM = mountCarrier.dom;
-        mountCarrier.dom = dom;
-        if (this.tag === 5) {
-            initChildren(this);
-        }
-        insertElement(this, beforeDOM);
-        if (this.tag === 5) {
-            this.attr();
-            updateQueue.push(this);
-        }
-    },
-    _isMounted: returnFalse,
-    attr() {
-        let { type, props, lastProps, stateNode: dom } = this;
-        diffProps(dom, lastProps || emptyObject, props, this);
-        if (formElements[type]) {
-            inputControll(this, dom, props);
-        }
-    },
-    resolve() {
-        this._isMounted = returnTrue;
-        Refs.fireRef(this, this.stateNode, this._reactInternalFiber);
-    },
-    dispose() {
-        Refs.fireRef(this, null, this._reactInternalFiber);
-    }
-};
--- src/dispose.js
+++ src/dispose.js
@@ -1,6 +1,4 @@
 import { options } from "./util";
-import { removeElement } from "./browser";
-
 export const topFibers = [];
 export const topNodes = [];
 
@@ -14,61 +12,7 @@ export function disposeVnode(fiber, updateQueue, silent) {
                 topNodes.splice(i, 1);
             }
         }
-      
-        if(fiber._return){
-            let dom = fiber._return.stateNode;
-            delete dom.__events;
-        }
-        if (fiber.tag < 4) {
-            disposeComponent(fiber, updateQueue, silent);
-        } else {
-            if (fiber.tag === 5) {
-                disposeElement(fiber, updateQueue, silent);
-            }
-            updateQueue.push({
-                node: fiber.stateNode,
-                vnode: fiber,
-                transition:remove
-            });
-        }
     }
-}
-function remove(){
-    this.vnode._disposed = true;
-    delete this.vnode.stateNode;
-    removeElement(this.node);
-}
-function disposeElement(fiber, updateQueue, silent) {
-    if (!silent) {
-        // fiber.addState("dispose");
-        fiber._states = ["dispose"];
-        updateQueue.push(fiber);
-    } else {
-        if (fiber._isMounted && fiber._isMounted()) {
-            fiber._states = ["dispose"];
-            updateQueue.push(fiber);
-        }
-    }
-    disposeChildren(fiber._children, updateQueue, silent);
-}
-
-function disposeComponent(fiber, updateQueue, silent) {
-    let instance = fiber.stateNode;
-    if (!instance) {
-        //没有实例化
-        return;
-    }
-    if (!silent) {
-        //  fiber.addState("dispose");
-        fiber._states = ["dispose"];
-        updateQueue.push(fiber);
-    } else if (fiber._isMounted()) {
-        fiber._states = ["dispose"];
-        updateQueue.push(fiber);
-    }
-
-    fiber._mountCarrier = fiber._mountPoint = NaN; //用null/undefined会碰到 xxx[0]抛错的问题
-    disposeChildren(fiber._children, updateQueue, silent);
 }
 
 export function disposeChildren(children, updateQueue, silent) {
