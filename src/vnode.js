bb2ba165d32c11aef74b28974c5c94b060e28a21
rubylouvre <cheng19840218@gmail.com>
2018-03-25 21:14:23 +0800

    移动位置

--- src/vnode.js
+++ src/vnode.js
@@ -1,22 +1,6 @@
 import { typeNumber, options, REACT_ELEMENT_TYPE } from "./util";
 import { Refs } from "./Refs";
 
-/*
- IndeterminateComponent = 0; // 不用
- FunctionalComponent = 1;
- ClassComponent = 2;
- HostRoot = 3; // 不用
- HostPortal = 4; // 不用
- HostComponent = 5; 
- HostText = 6;
- CallComponent = 7; // 不用
- CallHandlerPhase = 8;// 不用
- ReturnComponent = 9;// 不用
- Fragment = 10;// 不用
- Mode = 11; // 不用
- ContextConsumer = 12;// 不用
- ContextProvider = 13;// 不用
-*/
 export function Vnode(type, tag, props, key, ref) {
     this.type = type;
     this.tag = tag;
