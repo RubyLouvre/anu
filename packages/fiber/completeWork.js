1832be4517b36e88a93f32f9ff84ef9d3bf5bcbf
rubylouvre <cheng19840218@gmail.com>
2018-04-21 22:45:39 +0800

    修正AnuPortal的情况

--- packages/fiber/completeWork.js
+++ packages/fiber/completeWork.js
@@ -1,5 +1,7 @@
 import { PLACE } from "./effectTag";
 import { __push } from "react-core/util";
+import { AnuPortal } from "react-core/createPortal";
+
 /**
  * 此方法主要是用于收集虚拟DOM上的各种任务（sideEffect）,并且为元素虚拟DOM指定插入点
  * 如果Fiber存在updateFail＝true属性，那么只收集它的元素虚拟DOM，并且它只有
@@ -31,7 +33,9 @@ export function collectEffects(fiber, updateFail, isTop) {
             child.insertPoint = child.parent.insertPoint;
             child.parent.insertPoint = child.stateNode;
         } else {
-            child.insertPoint = child.parent.insertPoint;
+            if(child.type != AnuPortal ){
+               child.insertPoint = child.parent.insertPoint;
+            }
         }
         if (updateFail || child.updateFail) {
            
