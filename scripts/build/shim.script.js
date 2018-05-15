8a7dcc509349d01fd8da22e08e57e10aba4d380b
RubyLouvre <1669866773@qq.com>
2018-05-15 12:06:03 +0800

    update

--- scripts/build/shim.script.js
+++ scripts/build/shim.script.js
@@ -7,7 +7,7 @@ var anuDir = path.join(projectDir, "./dist/React.js");
 var anuSource = fs.readFileSync(anuDir, "utf-8");
 var anuRefactor = anuSource.replace(/Object\.freeze/g, "extend").replace(/\/\/freeze_start([\s\S]+?)freeze_end/, "");
 fs.writeFileSync(anuDir, anuRefactor, { encoding: "utf8" });
-fs.writeFileSync( path.join(projectDir, "../antd-test/node_modules/qreact/dist/React.js"), anuRefactor, { encoding: "utf8" });
+fs.writeFileSync( path.join(projectDir, "../antd-test/node_modules/anujs/dist/React.js"), anuRefactor, { encoding: "utf8" });
 
 
 console.log("对React瘦身完毕"); // eslint-disable-line
