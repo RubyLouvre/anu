b9ae847019f0bf45105b8a873463be1be072b3db
rubylouvre <cheng19840218@gmail.com>
2018-05-22 23:00:09 +0800

    update

--- packages/fiber/version.js
+++ packages/fiber/version.js
@@ -1,6 +1,6 @@
 
-export function addVersion(a) {
-    var map = a.parent.__map__;
+export function insertList(a) {
+    var v = a.version, parent = a.parent;
     var list = [], b = a;
     while (a) {
         list.unshift(a.index);
@@ -10,25 +10,51 @@ export function addVersion(a) {
             break;
         }
     }
-    map[list.join(".")] = b;
-}
-export function toList(obj){
-    var list = [];
-    for(var i in obj){
-        list.push(obj[i]);
+    var nv = list.join(".");
+    if (nv !== v) {
+        b.version = nv;
+        var head = parent.__head;
+        if (!head) {
+            parent.__head = b;
+        } else {
+            _insertList(parent, head, b);
+        }
     }
-    return list.sort(versionCompare);
 }
-export function removeVersion(a) {
-    var map = a.parent.__map__;
-    for (var i in map) {
-        if (map[i] === a) {
-            delete map[i];
+export function _insertList(parent, head, fiber) {
+    //如果fiber在head的前面
+    if (versionCompare(head, fiber) == 1) {
+        fiber.next = head;
+        parent.__head = fiber;
+        return;
+    }
+    while (head.next) {
+        //fiber在head.next前面，在head后面
+        if (versionCompare(head.next, fiber) == 1) {
+            var b = head.next;
+            head.next = fiber;
+            fiber.next = b;
             break;
         }
     }
- 
 }
+export function removeList(fiber) {
+    var parent = fiber.parent;
+    var head = parent.__head;
+    if (head) {
+        if (head.stateNode == fiber.stateNode) {
+            parent.__head = head.next;
+            return;
+        }
+        while (head.next) {
+            if (head.next.stateNode == fiber.stateNode) {
+                head.next = head.next.next;
+                break;
+            }
+        }
+    }
+}
+
 export function versionCompare(stra, strb) {
     var straArr = stra.split(".");
     var strbArr = strb.split(".");
