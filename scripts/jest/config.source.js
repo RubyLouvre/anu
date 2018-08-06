4fc433bc1228b05d13f3581b662814aeb474900d
RubyLouvre <1669866773@qq.com>
2018-08-06 19:28:35 +0800

    fix jest

--- scripts/jest/config.source.js
+++ scripts/jest/config.source.js
@@ -1,22 +1,22 @@
-"use strict";
+'use strict';
 
 module.exports = {
     haste: {
-        hasteImplModulePath: require.resolve("./noHaste.js"),
+        hasteImplModulePath: require.resolve('./noHaste.js'),
     },
     modulePathIgnorePatterns: [
-        "<rootDir>/scripts/rollup/shims/",
-        "<rootDir>/scripts/bench/",
+        '<rootDir>/scripts/rollup/shims/',
+        '<rootDir>/scripts/bench/',
     ],
-
-
-    setupFiles: [require.resolve("./setupEnvironment.js")],
-    setupTestFrameworkScriptFile: require.resolve("./setupTests.js"),
+    testURL:'http://localhost/',
+    // 'testEnvironment': 'node',   
+    setupFiles: [require.resolve('./setupEnvironment.js')],
+    setupTestFrameworkScriptFile: require.resolve('./setupTests.js'),
     // Only include files directly in __tests__, not in nested folders.
-    testRegex: "/__tests__/[^/]*(\\.js|\\.coffee|[^d]\\.ts)$",
-    moduleFileExtensions: ["js", "json", "node", "coffee", "ts"],
+    testRegex: '/__tests__/[^/]*(\\.js|\\.coffee|[^d]\\.ts)$',
+    moduleFileExtensions: ['js', 'json', 'node', 'coffee', 'ts'],
     rootDir: process.cwd(),
-    roots: ["<rootDir>/packages", "<rootDir>/scripts"],
-    collectCoverageFrom: ["packages/**/*.js"],
-    timers: "fake",
+    roots: ['<rootDir>/packages', '<rootDir>/scripts'],
+    collectCoverageFrom: ['packages/**/*.js'],
+    timers: 'fake',
 };
