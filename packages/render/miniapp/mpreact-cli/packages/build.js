fb3f3ee8ddc62686c8dfadf606201ab9254ac828
YangTao <blue.yang@qunar.com>
2018-08-07 11:56:50 +0800

    更新translator目录

--- packages/render/miniapp/mpreact-cli/packages/build.js
+++
@@ -1,9 +0,0 @@
-
-
-const path = require('path');
-const transform = require('./translator/index');
-const build = (arg)=>{
-    transform(arg);
-}
-
-module.exports = build;
--- packages/render/miniapp/translator/helpers/exportExpr.js
+++
@@ -1,9 +0,0 @@
-const template = require("babel-template");
-
-module.exports = function xxx(name, isDefault) {
-    if (isDefault == true) {
-        return template(`module.exports.default = ${name};`)();
-    } else {
-        return template(`module.exports["${name}"] = ${name};`)();
-    }
-};
