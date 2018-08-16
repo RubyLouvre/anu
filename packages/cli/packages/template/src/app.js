01962048e7eb4483e12bf474a5b1868ade35aa28
blue.yang <blue.yang@qunar.com>
2018-08-16 10:31:54 +0800

    去除app.js中不必要的page 路由配置,以import来分析路由

--- packages/cli/packages/template/src/app.js
+++ packages/cli/packages/template/src/app.js
@@ -29,33 +29,6 @@ import './app.less';
 
 class Demo extends React.Component {
     config = {
-        pages: [
-            'pages/index/index',
-            'pages/demo/form/index/index',
-            'pages/demo/form/button/index',
-            'pages/demo/form/checkbox/index',
-            'pages/demo/form/input/index',
-            'pages/demo/form/slider/index',
-            'pages/demo/form/picker/index',
-            'pages/demo/form/radio/index',
-            'pages/demo/form/textarea/index',
-            'pages/demo/form/label/index',
-            'pages/demo/baseContent/base/index',
-            'pages/demo/view/index/index',
-            'pages/demo/view/view/index',
-            'pages/demo/view/scrollView/index',
-            'pages/demo/view/movableView/index',
-            'pages/demo/view/swiper/index',
-            'pages/demo/media/index/index',
-            'pages/demo/media/audio/index',
-            'pages/demo/media/image/index',
-            'pages/demo/media/video/index',
-            'pages/demo/media/camera/index',
-            "pages/demo/api/index",
-            'pages/demo/extend/index',
-            'pages/demo/extend/Dog',
-            'pages/demo/extend/Husky'
-        ],
         window: {
             backgroundTextStyle: 'light',
             navigationBarBackgroundColor: '#0088a4',
