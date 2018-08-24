c0f39a3c0efaa667daea1689e91e5e0a3e3c1682
blue.yang <blue.yang@qunar.com>
2018-08-24 10:40:06 +0800

    support relative module path

--- packages/cli/packages/translator/helpers/resolveAlias.js
+++ packages/cli/packages/translator/helpers/resolveAlias.js
@@ -1,12 +1,21 @@
 const path = require('path');
 const cwd = process.cwd();
+
+
+const log = console.log;
 module.exports = (nPath, modules)=>{
     let aliasField = require( path.join(cwd, 'package.json') ).mpreact.alias;
     if(!aliasField) return;
     for(let key in aliasField){
+        
+        let dir = path.parse(nPath.node.source.value).dir;
+        if(dir){
+            return;
+        }
+        
 
         let aliasPath = aliasField[key];  
-        let from = path.dirname(path.join(cwd, modules.current)); 
+        let from = path.dirname(path.join(cwd, modules.current));
         let to = path.dirname(path.join(cwd, aliasPath));
 
         let {name} = path.parse(aliasPath);
