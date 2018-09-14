e5c406bdbb076e295c6463cfc34d887cb0b54ba6
RubyLouvre <1669866773@qq.com>
2018-09-14 10:57:05 +0800

    update

--- aa1/dist/npm/weapp-async-await/index.js
+++
@@ -1,6 +0,0 @@
-const Promise = require('./promise-polyfill/lib/index.js');
-const regeneratorRuntime = require('./regenerator-runtime/runtime.js');
-module.exports = {
-    Promise: Promise,
-    regeneratorRuntime: regeneratorRuntime
-}
--- aa1/src/components/Cat/index.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line
-import React from '@react';
-
-export default function Cat(props){
-    return <div>Cat: {props.id}_{props.content}</div>;
-}
