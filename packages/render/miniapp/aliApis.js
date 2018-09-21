5b9a14013479d607e61ccbbe0fcfda8a0989e943
gaoxiaolin.gao <gaoxiaolin.gao@qunar.com>
2018-09-21 17:30:48 +0800

    feat: ali apis

--- packages/render/miniapp/aliApis.js
+++ packages/render/miniapp/aliApis.js
@@ -1,1 +1,28 @@
-export var aliApis = {};
+export var aliApis = function(api) {
+  return {
+    showModal: function() {
+      const { cancelText, confirmText } = arguments[0];
+      arguments[0].cancelButtonText = cancelText;
+      arguments[0].confirmButtonText = confirmText;
+      return api.confirm.apply(api, arguments);
+    },
+
+    showActionSheet: function() {
+      const { itemList } = arguments[0];
+      arguments[0].items = itemList;
+      return api.showActionSheet.apply(api, arguments);
+    },
+    showActionSheet: function() {
+      const { title, icon } = arguments[0];
+      arguments[0].content = title;
+      arguments[0].type = icon;
+      return api.showActionSheet.apply(api, arguments);
+    },
+
+    showLoading: function() {
+      const { title } = arguments[0];
+      arguments[0].content = title;
+      return api.showLoading.apply(api, arguments);
+    }
+  };
+};
