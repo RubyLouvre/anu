9634988084d529e3f689e7443121cb556fb35ee1
leiy.yao <leiy.yao@qunar.com>
2018-09-29 17:38:20 +0800

    百度界面模块api兼容

--- packages/render/miniapp/buApis.js
+++ packages/render/miniapp/buApis.js
@@ -1,14 +1,14 @@
 export var buApis = function(api) {
     return {
-        showActionSheet: function (options) {
-            var { success, complete } = options;
-            success && (options.success = function (res = {}) {
+        showActionSheet: function(a) {
+            var { success, complete } = a;
+            success && (a.success = function (res = {}) {
                 success.call(api, { index: res.tapIndex });
             });
-            complete && (options.complete = function (res = {}) {
+            complete && (a.complete = function (res = {}) {
                 complete.call(api, { index: res.tapIndex });
             });
-            return api.showActionSheet.call(api, options);
+            return api.showActionSheet.apply(api, arguments);
         }
     };
 };
