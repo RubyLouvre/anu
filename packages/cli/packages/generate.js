a627f51034e8b3e655ba1606d3a35f23b4f7f808
blue.yang <blue.yang@qunar.com>
2018-09-29 12:09:49 +0800

    update bin

--- packages/cli/packages/generate.js
+++ packages/cli/packages/generate.js
@@ -5,21 +5,21 @@ const nPath = require('path');
 const chalk = require('chalk');
 const cwd = process.cwd();
 
-const print = (prefix, msg, type) => {
+const print = (prefix, msg) => {
     // eslint-disable-next-line
-    console.log(chalk.green(`${prefix} ${msg}`), type);
+    console.log(chalk.green(`${prefix} ${msg}`));
 };
 
 module.exports = ()=>{
     utils.on('build', ()=>{
         while (queue.length){
-            let {code, path, type } = queue.shift();
+            let {code, path } = queue.shift();
             fs.ensureFileSync(path);
             fs.writeFile(path, code, err => {
                 if (err){
                     print('build fail:', nPath.relative(cwd, path));
                 } else {
-                    print('build success:', nPath.relative(cwd, path), `   ${type}`);
+                    print('build success:', nPath.relative(cwd, path));
                 }
             });
         }
