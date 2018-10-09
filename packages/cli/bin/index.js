eeb7c18ec89586722fe9d49f4777b6f1fbe7398a
blue.yang <blue.yang@qunar.com>
2018-10-09 20:59:27 +0800

    update cli bin

--- packages/cli/bin/index.js
+++ packages/cli/bin/index.js
@@ -6,25 +6,41 @@ if ( Number(process.version.match(/v(\d+)/)[1]) < 8) {
     console.log( `当前nodejs版本为 ${chalk.red(process.version)}, 请保证 >= ${chalk.bold(8)}`);
     process.exit(1);
 }
-const config = require('../packages/config');
-
 const program = require('commander');
+
+program
+    .name('mpreact')
+    .usage('<commond>')
+    .version(require('../package.json').version, '-v, --version')
+
+program
+    .command('init <project-name>')
+    .description('初始化项目')
+
+program
+    .command('watch:[wx|ali|bu|quick]')
+    .description('监听[ 微信小程序 | 支付宝小程序 | 百度只能小程序 | 快应用]')
+
+program
+    .command('build:[wx|ali|bu|quick]')
+    .description('构建[ 微信小程序 | 支付宝小程序 | 百度只能小程序 | 快应用]')
+
+program
+    .command('update')
+    .description('update cli')
+
+
+program.parse(process.argv);
+if(program.args.length === 0) program.help();
+
+
+const config = require('../packages/config');
+const args = program.args;
 function getBuildType(args){
-    let argList = args[0].split(':');
-    let type = argList[1];
+    let type = args[0].split(':')[1];
     type = !type ? 'wx' : type.toLowerCase();
     return type;
 }
-program
-    .name('mpreact')
-    .version(require('../package.json').version, '-v, --version')
-    .command('watch:[wx|ali|bu|quick]', '监听[ 微信小程序 | 支付宝小程序 | 百度只能小程序 | 快应用]')
-    .command('build:[wx|ali|bu|quick]', '构建[ 微信小程序 | 支付宝小程序 | 百度只能小程序 | 快应用]')
-    .parse(process.argv);
-let args = program.args;
-
-
-
 
 /* eslint-disable */
 if (args[0] === 'init' && typeof args[1] === 'undefined') {
@@ -39,11 +55,8 @@ if (args[0] === 'init' && typeof args[1] === 'undefined') {
     process.exit(1);
 }
 
-
 let buildType = getBuildType(args);
-
-process.env.ANU_ENV = buildType;
-
+/* eslint-disable */
 if(!config[buildType]){
     let type = args[0].split(':');
     console.log(
@@ -55,6 +68,11 @@ if(!config[buildType]){
     console.log(chalk.green(`4.快应用:            mpreact ${type[0]}:quick`));
     process.exit(1);
 }
+
+
+
+process.env.ANU_ENV = buildType;
+
 if(!config[buildType].support ){
     console.log(
         chalk.red(config[buildType].notSopportResText)
@@ -79,6 +97,9 @@ switch(commond){
     case 'init':
         require('../packages/init')(args[1]);
         break;  
+    case 'update':
+        require('../packages/updateCli')();
+        break;
     default:
         console.log(chalk.green('初始化项目: mpreact init <project-name>'));      
 }
--- packages/cli/bin/nanachi.js
+++ packages/cli/bin/nanachi.js
@@ -6,44 +6,57 @@ if ( Number(process.version.match(/v(\d+)/)[1]) < 8) {
     console.log( `当前nodejs版本为 ${chalk.red(process.version)}, 请保证 >= ${chalk.bold(8)}`);
     process.exit(1);
 }
-const config = require('../packages/config');
-
 const program = require('commander');
+
+program
+    .name('nanachi')
+    .usage('<commond>')
+    .version(require('../package.json').version, '-v, --version')
+
+program
+    .command('init <project-name>')
+    .description('初始化项目')
+
+program
+    .command('watch:[wx|ali|bu|quick]')
+    .description('监听[ 微信小程序 | 支付宝小程序 | 百度只能小程序 | 快应用]')
+
+program
+    .command('build:[wx|ali|bu|quick]')
+    .description('构建[ 微信小程序 | 支付宝小程序 | 百度只能小程序 | 快应用]')
+
+program
+    .command('update')
+    .description('update cli')
+
+
+program.parse(process.argv);
+if(program.args.length === 0) program.help();
+
+
+const config = require('../packages/config');
+const args = program.args;
 function getBuildType(args){
-    let argList = args[0].split(':');
-    let type = argList[1];
+    let type = args[0].split(':')[1];
     type = !type ? 'wx' : type.toLowerCase();
     return type;
 }
-program
-    .name('nanachi')
-    .version(require('../package.json').version, '-v, --version')
-    .option('-h,--help', '')
-    .command('watch:[wx|ali|bu|quick]', '监听[ 微信小程序 | 支付宝小程序 | 百度只能小程序 | 快应用]')
-    .command('build:[wx|ali|bu|quick]', '构建[ 微信小程序 | 支付宝小程序 | 百度只能小程序 | 快应用]')
-    .on('--help', ()=>{})
-    .parse(process.argv);
-let args = program.args;
-
 
 /* eslint-disable */
 if (args[0] === 'init' && typeof args[1] === 'undefined') {
     console.error('请指定项目名称');
     console.log(
-        `  ${chalk.cyan(program.name())} ${chalk.green('<project-name>')}\n`
+        `  ${chalk.cyan(program.name())} init ${chalk.green('<project-name>')}\n`
     );
     console.log('例如:\n');
     console.log(
-        `  ${chalk.cyan(program.name())} ${chalk.green('nanachi-app')}`
+        `  ${chalk.cyan(program.name())} init ${chalk.green('nanachi-app')}`
     );
     process.exit(1);
 }
 
-
 let buildType = getBuildType(args);
-
-process.env.ANU_ENV = buildType;
-
+/* eslint-disable */
 if(!config[buildType]){
     let type = args[0].split(':');
     console.log(
@@ -55,6 +68,11 @@ if(!config[buildType]){
     console.log(chalk.green(`4.快应用:            nanachi ${type[0]}:quick`));
     process.exit(1);
 }
+
+
+
+process.env.ANU_ENV = buildType;
+
 if(!config[buildType].support ){
     console.log(
         chalk.red(config[buildType].notSopportResText)
@@ -64,13 +82,11 @@ if(!config[buildType].support ){
 config['buildType'] = buildType;
 
 
-
 let commond = args[0];
 if(/\:/.test(commond)){
     //<watch|build>:
     commond = commond.split(':')[0];
 }
-    
 switch(commond){
     case 'watch':
         require('../packages/index')('watch', buildType);
@@ -81,6 +97,9 @@ switch(commond){
     case 'init':
         require('../packages/init')(args[1]);
         break;  
+    case 'update':
+        require('../packages/updateCli')();
+        break;
     default:
         console.log(chalk.green('初始化项目: nanachi init <project-name>'));      
 }
