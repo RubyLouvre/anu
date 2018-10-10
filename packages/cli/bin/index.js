80b5e481ca0bf68c24995f59a3e273b81588eca2
祝鑫奔 <rolandreed.cn@gmail.com>
2018-10-10 14:43:31 +0800

    typo

--- packages/cli/bin/index.js
+++ packages/cli/bin/index.js
@@ -1,42 +1,42 @@
 #!/usr/bin/env node
 'use strict';
 const chalk = require('chalk');
-if ( Number(process.version.match(/v(\d+)/)[1]) < 8) {
+const semver = require('semver');
+const program = require('commander');
+
+if (semver.lt(process.version, '8')) {
     // eslint-disable-next-line
-    console.log( `当前nodejs版本为 ${chalk.red(process.version)}, 请保证 >= ${chalk.bold(8)}`);
+    console.log(
+        chalk`mpreact only support {green.bold v8.0.0} or later (current v{green.bold ${
+            process.version
+        }}) of Node.js`
+    );
     process.exit(1);
 }
-const program = require('commander');
 
 program
     .name('mpreact')
-    .usage('<commond>')
-    .version(require('../package.json').version, '-v, --version')
+    .usage('<command>')
+    .version(require('../package.json').version, '-v, --version');
 
-program
-    .command('init <project-name>')
-    .description('初始化项目')
+program.command('init <project-name>').description('初始化项目');
 
 program
     .command('watch:[wx|ali|bu|quick]')
-    .description('监听[ 微信小程序 | 支付宝小程序 | 百度只能小程序 | 快应用]')
+    .description('监听[ 微信小程序 | 支付宝小程序 | 百度只能小程序 | 快应用]');
 
 program
     .command('build:[wx|ali|bu|quick]')
-    .description('构建[ 微信小程序 | 支付宝小程序 | 百度只能小程序 | 快应用]')
+    .description('构建[ 微信小程序 | 支付宝小程序 | 百度只能小程序 | 快应用]');
 
-program
-    .command('update')
-    .description('update cli')
-
+program.command('update').description('update cli');
 
 program.parse(process.argv);
-if(program.args.length === 0) program.help();
-
+if (program.args.length === 0) program.help();
 
 const config = require('../packages/config');
 const args = program.args;
-function getBuildType(args){
+function getBuildType(args) {
     let type = args[0].split(':')[1];
     type = !type ? 'wx' : type.toLowerCase();
     return type;
@@ -46,7 +46,9 @@ function getBuildType(args){
 if (args[0] === 'init' && typeof args[1] === 'undefined') {
     console.error('请指定项目名称');
     console.log(
-        `  ${chalk.cyan(program.name())} init ${chalk.green('<project-name>')}\n`
+        `  ${chalk.cyan(program.name())} init ${chalk.green(
+            '<project-name>'
+        )}\n`
     );
     console.log('例如:\n');
     console.log(
@@ -57,37 +59,30 @@ if (args[0] === 'init' && typeof args[1] === 'undefined') {
 
 let buildType = getBuildType(args);
 /* eslint-disable */
-if(!config[buildType]){
+if (!config[buildType]) {
     let type = args[0].split(':');
-    console.log(
-        chalk.red('请检查命令是否正确')
-    )
-    console.log(chalk.green(`1.微信小程序:        mpreact ${type[0]}`))
+    console.log(chalk.red('请检查命令是否正确'));
+    console.log(chalk.green(`1.微信小程序:        mpreact ${type[0]}`));
     console.log(chalk.green(`2.百度智能小程序:    mpreact ${type[0]}:bu`));
     console.log(chalk.green(`3.支付宝小程序:      mpreact ${type[0]}:ali`));
     console.log(chalk.green(`4.快应用:            mpreact ${type[0]}:quick`));
     process.exit(1);
 }
 
-
-
 process.env.ANU_ENV = buildType;
 
-if(!config[buildType].support ){
-    console.log(
-        chalk.red(config[buildType].notSopportResText)
-    )
+if (!config[buildType].support) {
+    console.log(chalk.red(config[buildType].notSupportResText));
     process.exit(1);
 }
 config['buildType'] = buildType;
 
-
-let commond = args[0];
-if(/\:/.test(commond)){
+let command = args[0];
+if (/\:/.test(command)) {
     //<watch|build>:
-    commond = commond.split(':')[0];
+    command = command.split(':')[0];
 }
-switch(commond){
+switch (command) {
     case 'watch':
         require('../packages/index')('watch', buildType);
         break;
@@ -96,10 +91,10 @@ switch(commond){
         break;
     case 'init':
         require('../packages/init')(args[1]);
-        break;  
+        break;
     case 'update':
         require('../packages/updateCli')();
         break;
     default:
-        console.log(chalk.green('初始化项目: mpreact init <project-name>'));      
+        console.log(chalk.green('初始化项目: mpreact init <project-name>'));
 }
