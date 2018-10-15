caea548a241edcc4a6779ef5d26ff9f0c56cb08b
RubyLouvre <1669866773@qq.com>
2018-10-15 11:34:50 +0800

    更新

--- packages/cli/packages/wxHelpers/render.js
+++ packages/cli/packages/wxHelpers/render.js
@@ -79,7 +79,6 @@ exports.exit = function(astPath, type, componentName, modules) {
                         // 生命周期函数，可以为函数，或一个在methods段中定义的方法名
                         attached: function (e) { 
                         },
-                        moved: function () { },
                         detached: function () { },
                       },
                     
