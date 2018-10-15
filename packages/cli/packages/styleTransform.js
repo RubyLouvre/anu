5d52a37816e1356ee36813ec05d2e9f86258fa88
blue.yang <blue.yang@qunar.com>
2018-10-15 18:02:59 +0800

    处理scss情况下component样式写入app中bug

--- packages/cli/packages/styleTransform.js
+++ packages/cli/packages/styleTransform.js
@@ -3,7 +3,9 @@ const cwd = process.cwd();
 const queue = require('./queue');
 const config = require('./config');
 const utils = require('./utils');
+const fs = require('fs-extra');
 const validateStyle = require('./validateStyle');
+const appSassStyleFileNameReg = /app\.(scss|sass)$/;
 
 
 const isLess = (filePath)=>{
@@ -53,9 +55,28 @@ const compileLess = (filePath, originalCode)=>{
 
 const renderSass = (filePath, originalCode)=>{
     let sass = require(path.join(cwd, 'node_modules', 'node-sass'));
+    
+    /**
+     * node-sass配置{data : originalCode}处理@import语句比较繁琐，所以用{file: filePath},
+     * 但app样式中可能存在动态分析插入的@import引用component样式，所以先写入一个隐藏文件, file api读取的是隐藏文件目录
+     */
+    if(appSassStyleFileNameReg.test(filePath)){
+        try {
+            var hideAppStylePath = filePath.replace(/app\.(scss|sass)$/, '.app.$1');
+            fs.ensureFileSync(hideAppStylePath);
+            fs.writeFileSync(hideAppStylePath, originalCode)
+        } catch(err) {
+            if (err) {
+                console.log(err);
+            }
+        }
+    }
+   
+    
+   
     sass.render(
         {
-            file: filePath
+            file: appSassStyleFileNameReg.test(filePath) ? hideAppStylePath : filePath
         },
         (err, res) => {
             if (err) throw err;
@@ -66,6 +87,17 @@ const renderSass = (filePath, originalCode)=>{
                 type: 'css'
             });
             utils.emit('build');
+            
+
+            if (appSassStyleFileNameReg.test(filePath)) {
+                fs.remove(hideAppStylePath, (err)=>{
+                    if(err){
+                        console.log(err);
+                    }
+                })
+            }
+           
+            
         }
     );
 };
