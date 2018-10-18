b8c9369829219a19b6e168838d536e6e79449116
gaoxiaolin.gao <gaoxiaolin.gao@qunar.com>
2018-10-18 15:13:13 +0800

    fix: setNavigationBar

--- packages/render/miniapp/aliApis.js
+++ packages/render/miniapp/aliApis.js
@@ -1,44 +1,36 @@
 export var aliApis = function(api) {
-    return {
-        // 交互
-        showModal: function _(a) {
-            a.cancelButtonText = a.cancelText;
-            a.confirmButtonText = a.confirmText;
-            return api.confirm.apply(api, arguments);
-        },
-        showActionSheet: function _(a) {
-            a.items = a.itemList;
-            return api.showActionSheet.apply(api, arguments);
-        },
-        showToast: function _(a) {
-            a.content = a.title;
-            a.type = a.icon;
-            return api.showToast.apply(api, arguments);
-        },
-        showLoading: function _(a) {
-            a.content = a.title;
-            return api.showLoading.apply(api, arguments);
-        },
-        // 导航类
-        setNavigationBarTitle: function _(a) {
-            a.image = null;
-            a.backgroundColor = null;
-            a.borderBottomColor = null;
-            a.reset = null;
-            return api.setNavigationBar.apply(api, arguments);
-        },
-        setNavigationBarColor: function _(a) {
-            a.image = null;
-            a.title = null;
-            a.borderBottomColor = null;
-            a.reset = null;
-            return api.setNavigationBar.apply(api, arguments);
-        },
-        // 图片保存到本地
-        saveImageToPhotosAlbum: function _(a) {
-            a.url = a.filePath;
-            return api.saveImage.apply(api, arguments);
-        },
+  return {
+    // 交互
+    showModal: function _(a) {
+      a.cancelButtonText = a.cancelText;
+      a.confirmButtonText = a.confirmText;
+      return api.confirm(a);
+    },
+    showActionSheet: function _(a) {
+      a.items = a.itemList;
+      return api.showActionSheet(a);
+    },
+    showToast: function _(a) {
+      a.content = a.title;
+      a.type = a.icon;
+      return api.showToast(a);
+    },
+    showLoading: function _(a) {
+      a.content = a.title;
+      return api.showLoading(a);
+    },
+    // 导航类
+    setNavigationBarTitle: function _(a) {
+      return api.setNavigationBar(a);
+    },
+    setNavigationBarColor: function _(a) {
+      return api.setNavigationBar(a);
+    },
+    // 图片保存到本地
+    saveImageToPhotosAlbum: function _(a) {
+      a.url = a.filePath;
+      return api.saveImage.apply(api, arguments);
+    },
 
         // 图片预览
         previewImage: function _(a) {
@@ -76,80 +68,80 @@ export var aliApis = function(api) {
             return api.openLocation.apply(api, arguments);
         },
 
-        // 数据缓存
-        getStorageSync: function _(a) {
-            var k = {};
-            k.key = a;
-            arguments[0] = k;
-            let res = api.getStorageSync.apply(api, arguments);
-            return res.data || '';
-        },
-        setStorageSync: function _(a1, a2) {
-            var k = {};
-            k.key = a1;
-            k.data = a2;
-            arguments[0] = k;
-            api.setStorageSync.apply(api, arguments);
-        },
-        // 上传
-        uploadFile: function _(a) {
-            a.fileName = a.name;
-            return api.uploadFile.apply(api, arguments);
-        },
-        // 下载
-        downloadFile: function _(a) {
-            let fn = a['success'];
-            a['success'] = res => {
-                res.tempFilePath = res.apFilePath;
-                fn && fn(res);
-            };
-            return api.downloadFile.apply(api, arguments);
-        },
-        // 图片
-        chooseImage: function _(a) {
-            let fn = a['success'];
-            a['success'] = res => {
-                res.tempFilePaths = res.apFilePaths;
-                fn && fn(res);
-            };
-            return api.chooseImage.apply(api, arguments);
-        },
-        // 剪切板
-        getClipboardData: function _(a) {
-            let fn = a['success'];
-            a['success'] = res => {
-                res.data = res.text;
-                fn && fn(res);
-            };
-            return api.getClipboard.apply(api, arguments);
-        },
-        setClipboardData: function _(a) {
-            a.text = a.data;
-            return api.setClipboard.apply(api, arguments);
-        },
-        // 打电话
-        makePhoneCall: function _(a) {
-            a.number = a.phoneNumber;
-            return api.makePhoneCall.apply(api, arguments);
-        },
+    // 数据缓存
+    getStorageSync: function _(a) {
+      var k = {};
+      k.key = a;
+      arguments[0] = k;
+      let res = api.getStorageSync.apply(api, arguments);
+      return res.data || '';
+    },
+    setStorageSync: function _(a1, a2) {
+      var k = {};
+      k.key = a1;
+      k.data = a2;
+      arguments[0] = k;
+      api.setStorageSync.apply(api, arguments);
+    },
+    // 上传
+    uploadFile: function _(a) {
+      a.fileName = a.name;
+      return api.uploadFile.apply(api, arguments);
+    },
+    // 下载
+    downloadFile: function _(a) {
+      let fn = a['success'];
+      a['success'] = res => {
+        res.tempFilePath = res.apFilePath;
+        fn && fn(res);
+      };
+      return api.downloadFile.apply(api, arguments);
+    },
+    // 图片
+    chooseImage: function _(a) {
+      let fn = a['success'];
+      a['success'] = res => {
+        res.tempFilePaths = res.apFilePaths;
+        fn && fn(res);
+      };
+      return api.chooseImage.apply(api, arguments);
+    },
+    // 剪切板
+    getClipboardData: function _(a) {
+      let fn = a['success'];
+      a['success'] = res => {
+        res.data = res.text;
+        fn && fn(res);
+      };
+      return api.getClipboard.apply(api, arguments);
+    },
+    setClipboardData: function _(a) {
+      a.text = a.data;
+      return api.setClipboard.apply(api, arguments);
+    },
+    // 打电话
+    makePhoneCall: function _(a) {
+      a.number = a.phoneNumber;
+      return api.makePhoneCall.apply(api, arguments);
+    },
 
-        // 扫码
-        scanCode: function _(a) {
-            a.hideAlbum = a.onlyFromCamera;
-            a.type = (a.scanType && a.scanType[0].slice(0, -4)) || 'qr';
-            let fn = a['success'];
-            a['success'] = res => {
-                res.result = res.code;
-                fn && fn(res);
-            };
+    // 扫码
+    scanCode: function _(a) {
+      a.hideAlbum = a.onlyFromCamera;
+      a.type = (a.scanType && a.scanType[0].slice(0, -4)) || 'qr';
+      let fn = a['success'];
+      a['success'] = res => {
+        res.result = res.code;
+        fn && fn(res);
+      };
 
-            return api.scan.apply(api, arguments);
-        },
+      return api.scan.apply(api, arguments);
+    },
 
-        // 屏幕亮度
-        setScreenBrightness: function _(a) {
-            a.brightness = a.value;
-            return api.setScreenBrightness.apply(api, arguments);
-        }
-    };
+    // 屏幕亮度
+    setScreenBrightness: function _(a) {
+      a.brightness = a.value;
+      return api.setScreenBrightness.apply(api, arguments);
+    }
+  };
 };
