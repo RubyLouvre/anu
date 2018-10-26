225edc7a36b938893eecf7b456a208f7bf6e066b
RubyLouvre <1669866773@qq.com>
2018-10-26 11:47:19 +0800

    添加按平台注入组件的机制

--- packages/render/miniapp/registerPageQuick.js
+++ packages/render/miniapp/registerPageQuick.js
@@ -3,108 +3,10 @@ import { createElement } from 'react-core/createElement';
 import { noop } from 'react-core/util';
 import { eventSystem } from './eventSystemQuick';
 import { delayMounts, updateMiniApp } from './utils';
-var showMenuList = [
-    {
-        title: '保存桌面',
-        callback() {
-            createShortcut();
-        }
-    },
-    {
-        title: '关于',
-        callback() {
-            const router = require('@system.router');
-            const appInfo = require('@system.app').getInfo();
-            router.push({
-                uri: '/pages/About',
-                params: {
-                    name: appInfo.name,
-                    icon: appInfo.icon
-                }
-            });
-        }
-    },
-    {
-        title: '取消',
-        callback() {}
-    }
-];
-/**
- * 显示菜单
- */
-export function showMenu(shareObject) {
-    const prompt = require('@system.prompt');
-    let list =  showMenuList;
-    if (shareObject) {
-        list = [
-            {
-                title: '分享',
-                callback() {
-                    const share = require('@system.share');
-                    share.share({
-                        shareType: 0,
-                        title: '标题',
-                        summary: '摘要',
-                        imagePath: 'xxx/xxx/xxx/share.jpg',
-                        targetUrl: 'http://www.example.com',
-                        platforms: ['WEIBO'],
-                        success: function(data) {
-                            console.log('handling success');
-                        },
-                        fail: function(data, code) {
-                            console.log(`handling fail, code = ${code}`);
-                        }
-                    });
-                }
-            }
-        ].concat(showMenuList);
-    }
-    //分享转发 https://doc.quickapp.cn/features/service/share.html
-    prompt.showContextMenu({
-        itemList: list.map(function(el) {
-            return el.title;
-        }),
-        success: function(ret) {
-            var el = list[ret.index];
-            if (el) {
-                el.callback();
-            }
-        }
-    });
-}
+
 export var shareObject = {};
 export function getApp() {
     return shareObject.app;
-}
-/**
- * 创建桌面图标
- * 注意：使用加载器测试`创建桌面快捷方式`功能时，请先在`系统设置`中打开`应用加载器`的`桌面快捷方式`权限
- */
-export function createShortcut() {
-    const prompt = require('@system.prompt');
-    const shortcut = require('@system.shortcut');
-    shortcut.hasInstalled({
-        success: function(ret) {
-            if (ret) {
-                prompt.showToast({
-                    message: '已创建桌面图标'
-                });
-            } else {
-                shortcut.install({
-                    success: function() {
-                        prompt.showToast({
-                            message: '成功创建桌面图标'
-                        });
-                    },
-                    fail: function(errmsg, errcode) {
-                        prompt.showToast({
-                            message: `${errcode}: ${errmsg}`
-                        });
-                    }
-                });
-            }
-        }
-    });
 }
 
 export function registerPage(PageClass, path) {
@@ -118,6 +20,7 @@ export function registerPage(PageClass, path) {
         },
         dispatchEvent: eventSystem.dispatchEvent,
         onInit(query) {
+            shareObject.app = this.$app.$def || this.$app._def;
             instance = render(
                 createElement(PageClass, {
                     path: path,
@@ -169,5 +72,4 @@ function transmitData(pageClass, pagePath, reactInstance, quickInstance) {
     shareObject.pageConfig = Object.keys(pageConfig).length ? pageConfig: null;
     shareObject.pagePath = pagePath;
     shareObject.page = reactInstance;
-    shareObject.app = quickInstance.$app.$def || quickInstance.$app._def;
 }
