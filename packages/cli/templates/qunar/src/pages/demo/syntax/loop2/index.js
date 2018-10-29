46d1a8497ee310fbe96ccf90fc25543c4b423403
RubyLouvre <1669866773@qq.com>
2018-10-29 17:20:30 +0800

    fix bug

--- packages/cli/templates/qunar/src/pages/demo/syntax/loop2/index.js
+++ packages/cli/templates/qunar/src/pages/demo/syntax/loop2/index.js
@@ -1,7 +1,7 @@
 import React from '@react';
 import Fish from '@components/Fish/index';
 
-import './index./index.scss';
+import './index.scss';
 class P extends React.Component {
     constructor() {
         super();
