7ed60413dc5e74404e0436d4848c700b32a03b3c
gaoxiaolin.gao <gaoxiaolin.gao@qunar.com>
2018-11-05 20:08:12 +0800

    fix: quick api

--- packages/render/miniapp/api.quick.js
+++ packages/render/miniapp/api.quick.js
@@ -1,61 +1,62 @@
 import { noop } from 'react-core/util';
-const JSON_TYPE_STRING = 'json';
+import request from './quickApis/fetch.js';
+import {uploadFile, downloadFile} from './quickApis/request.js';
+import {setStorage,getStorage, removeStorage, clearStorage, setStorageSync, getStorageSync, removeStorageSync,clearStorageSync} from './quickApis/storage.js';
+import {getSavedFileInfo, getSavedFileList, removeSavedFile, saveFile} from './quickApis/file.js';
+import {setClipboardData, getClipboardData} from './quickApis/clipboard.js';
+import {getNetworkType, onNetworkStatusChange} from './quickApis/network.js';
+import {getSystemInfo} from './quickApis/device.js';
+import {chooseImage} from './quickApis/media.js';
+
 
 function createRouter(name) {
-    return function(obj) {
-        const router = require('@system.router');
-        const params = {};
-        const href = obj.url || obj.uri || '';
-        let uri = href.slice(href.indexOf('/pages') + 1);
-        uri = uri.replace(/\?(.*)/, function (a, b) {
-            b.split('=').forEach(function (k, v) {
-                params[k] = v;
-            });
-            return '';
-        }).replace(/\/index$/, '');
-        if (uri[0] !== '/') {
-            uri = '/' + uri;
-        }
-        router[name]({
-            uri,
-            params
+  return function(obj) {
+    const router = require('@system.router');
+    const params = {};
+    let uri = obj.url.slice(obj.url.indexOf('/pages') + 1);
+    uri = uri
+      .replace(/\?(.*)/, function(a, b) {
+        b.split('=').forEach(function(k, v) {
+          params[k] = v;
         });
-    };
+        return '';
+      })
+      .replace(/\/index$/, '');
+    router[name]({
+      uri,
+      params
+    });
+  };
 }
 
-function runFunction(fn, ...args) {
-    if (typeof fn == 'function') {
-        fn.call(null, ...args);
-    }
-}
 
 export var api = {
-    // 交互
-    showModal(obj) {
+  // 交互
+  showModal(obj) {
     // showCancel 默认值是 true
-        obj.showCancel = obj.showCancel === false ? false : true;
-        var buttons = [
-            {
-                text: obj.confirmText,
-                color: obj.confirmColor
-            }
-        ];
-        if (obj.showCancel) {
-            buttons.push({
-                text: obj.cancelText,
-                color: obj.cancelColor
-            });
-        }
-        obj.buttons = obj.confirmText ? buttons : [];
-        obj.message = obj.content;
-        delete obj.content;
-        let fn = obj['success'];
-        obj['success'] = res => {
-            res.confirm = !res.index;
-            fn && fn(res);
-        };
+    obj.showCancel = obj.showCancel === false ? false : true;
+    var buttons = [
+      {
+        text: obj.confirmText,
+        color: obj.confirmColor
+      }
+    ];
+    if (obj.showCancel) {
+      buttons.push({
+        text: obj.cancelText,
+        color: obj.cancelColor
+      });
+    }
+    obj.buttons = obj.confirmText ? buttons : [];
+    obj.message = obj.content;
+    delete obj.content;
+    let fn = obj['success'];
+    obj['success'] = res => {
+      res.confirm = !res.index;
+      fn && fn(res);
+    };
 
-        /*
+    /*
         title	String	否	标题
         message	String	否	内容
         buttons	Array	否	按钮的数组，按钮结构：{text:'text',color:'#333333'}，color可选：buttons的第1项为positive button；buttons的第2项（如果有）为negative button；buttons的第3项（如果有）为neutral button。最多支持3个button
@@ -63,124 +64,78 @@ export var api = {
         cancel	Function	否	取消回调
         complete Function	否	执行结束后的回调
         */
-        var prompt = require('@system.prompt');
-        prompt.showDialog(obj);
-    },
-    showToast(obj) {
-        var prompt = require('@system.prompt');
-        obj.message = obj.title;
-        obj.duration = obj.duration / 1000;
-        prompt.showToast(obj);
-    },
-    hideToast: noop,
-    showActionSheet(obj) {
-        var prompt = require('@system.prompt');
-        prompt.showContextMenu(obj);
-    },
+    var prompt = require('@system.prompt');
+    prompt.showDialog(obj);
+  },
+  showToast(obj) {
+    var prompt = require('@system.prompt');
+    obj.message = obj.title;
+    obj.duration = obj.duration / 1000;
+    prompt.showToast(obj);
+  },
+  hideToast: noop,
+  showActionSheet(obj) {
+    var prompt = require('@system.prompt');
+    prompt.showContextMenu(obj);
+  },
 
-    // 导航
-    navigateTo: createRouter('push'),
-    redirectTo: createRouter('replace'),
-    navigateBack: createRouter('back'),
-    // 震动
-    vibrateLong() {
-        var vibrator = require('@system.vibrator');
-        vibrator.vibrate();
-    },
-    vibrateShort() {
-        var vibrator = require('@system.vibrator');
-        vibrator.vibrate();
-    },
+  // 导航
+  navigateTo: createRouter('push'),
+  redirectTo: createRouter('replace'),
+  navigateBack: createRouter('back'),
+  // 震动
+  vibrateLong() {
+    var vibrator = require('@system.vibrator');
+    vibrator.vibrate();
+  },
+  vibrateShort() {
+    var vibrator = require('@system.vibrator');
+    vibrator.vibrate();
+  },
 
-    // 分享
-    share(obj) {
-        var share = require('@system.share');
-        share.share(obj);
-    },
+  // 分享
+  share(obj) {
+    var share = require('@system.share');
+    share.share(obj);
+  },
 
-    // 上传
-    uploadFile({
-        url,
-        filePath,
-        // 小米不支持
-        name,
-        header,
-        // 小米不支持
-        formData,
-        success,
-        fail,
-        complete
-    }) {
-        var request = require('@system.request');
-        var data = [];
-        Object.keys(formData).map(key => {
-            let value = formData[key];
-            let item = {
-                value,
-                name: key
-            };
-            data.push(item);
-        });
-        function successForMi({ code: statusCode, data }) {
-            success({
-                statusCode,
-                data
-            });
-        }
-        request.upload({
-            url,
-            header,
-            data,
-            files: [{ uri: filePath, name: name }],
-            success: successForMi,
-            fail,
-            complete
-        });
-    },
+  // 上传
+  uploadFile,
 
-    // 下载
-    downloadFile(obj) {
-        var request = require('@system.request');
-        request.download(obj);
-    },
-    // 网络请求
-    request({
-        url,
-        data,
-        header,
-        method,
-        dataType = JSON_TYPE_STRING,
-        // 小米不支持设置 responseType
-        // responseType,
-        success,
-        fail,
-        complete
-    }) {
-        const fetch = require('@system.fetch');
-        function onFetchSuccess({ code: statusCode, data, header: headers }) {
-            if (dataType === JSON_TYPE_STRING) {
-                try {
-                    data = JSON.parse(data);
-                } catch (error) {
-                    runFunction(fail, error);
-                }
-            }
+  // 下载
+  downloadFile,
+  // 网络请求
+  request,
+  // 二维码
+  scanCode({success, fail, complete}) {
+    const barcode = require('@system.barcode')
+    barcode.scan({
+      // 小米回调函数参数对象仅提供 result，不含 scanType、charSet 及 path
+      success,
+      fail,
+      cancel: fail,
+      complete
+    });
+  },
+  setStorage,
+  getStorage,
+  removeStorage,
+  clearStorage,
+  setStorageSync, 
+  getStorageSync, 
+  removeStorageSync,
+  clearStorageSync,
+  getSavedFileInfo,
+  getSavedFileInfo, getSavedFileList, removeSavedFile, saveFile,
+  setClipboardData, getClipboardData,
+  // 位置
+  getLocation(obj) {
+    const geolocation = require('@system.geolocation');
+    geolocation.getLocation(obj)
+  },
+  getNetworkType,
+  onNetworkStatusChange,
+  getSystemInfo,
+  chooseImage
 
-            success({
-                statusCode,
-                data,
-                headers
-            });
-        }
-
-        fetch.fetch({
-            url,
-            data,
-            header,
-            method,
-            success: onFetchSuccess,
-            fail,
-            complete
-        });
-    }
 };
