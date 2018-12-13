6347037fefaed2ba3823c44b733bd28d6c6e7cc9
jingjing.geng <jingjing.geng@qunar.com>
2018-12-13 19:17:31 +0800

    fix: 修改网易云模板bug

---
+++ packages/cli/templates/music/source/pages/recommend/index.js
@@ -0,0 +1,29 @@
+import React from '@react';
+import url from '../../utils/bsurl';
+let bsurl = url.bsurl;
+import './index.less';
+
+class Recommend extends React.Component {
+
+    // constructor() {
+    //     super();
+    //     this.state={
+
+    //     }
+    // };
+
+    // componentWillMount() {
+
+    // }
+
+    render() {
+        return (
+            <div>评论页面</div>
+        );
+    }
+
+
+}
+export default Recommend;
+
+
