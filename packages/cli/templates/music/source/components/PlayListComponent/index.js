d3a431d823462b46a11b9f803c80979d445a969e
jingjing.geng <jingjing.geng@qunar.com>
2018-12-14 21:14:51 +0800

    fix: 网易云音乐列表样式

--- packages/cli/templates/music/source/components/PlayListComponent/index.js
+++ packages/cli/templates/music/source/components/PlayListComponent/index.js
@@ -1,8 +1,8 @@
 import React from '@react';
 
 class PlayListComponent extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
     }
 
     render() {
