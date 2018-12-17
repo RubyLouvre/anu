11d8332cabde53014938353323d35b5e4639564b
shaoyudong <shaoyudong33@gmail.com>
2018-12-17 20:35:14 +0800

    fix

--- packages/cli/packages/stylesTransformer/postcssTransformLess.js
+++ packages/cli/packages/stylesTransformer/postcssTransformLess.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const postCss = require('postcss');
 const validateStyle = require('../validateStyle');
 const utils = require('../utils');
+const varReg = /@{?([a-zA-Z0-9-_.]+)}?/;
 
 //postcss插件: 清除注释
 const postCssRemoveComments = postCss.plugin('postcss-plugin-remove-comment', ()=>{
@@ -14,30 +15,39 @@ const postCssRemoveComments = postCss.plugin('postcss-plugin-remove-comment', ()
 });
 
 const postCssPluginLessVar = postCss.plugin('postCssPluginLessVar', ()=> {
-    function findValue(node, decl) {
+    function findVar(node, decl) {
         let find = false;
         let value;
         // 遍历variable 找出当前节点下变量定义
         node.walkAtRules(rule => {
-            if (rule.variable && rule.name === decl.value.split('@')[1]) {
-                find = true;
-                value = rule.value;
-            }
+            decl.value.replace(varReg, function(a, b) {
+                if (b && b === rule.name) {
+                    find = true;
+                    value = rule.value;
+                }
+                return a;
+            });
         });
         if (find && value) {
-            decl.value = value;
+            decl.value = decl.value.replace(varReg, value);
         }
         // 没找到或到达根节点则退出递归
         if (!find && node.type !== 'root') {
-            findValue(node.parent, decl);
+            findVar(node.parent, decl);
         }
     }
 
     return (root) => {
         root.walkDecls(decl => {
             // 找出变量
-            if (decl.value && decl.value.match(/(@{?[a-zA-Z0-9-_.]+}?)/g)) {
-                findValue(decl.parent, decl);
+            if (decl.value && decl.value.match(varReg)) {
+                findVar(decl.parent, decl);
+            }
+        });
+        // 移除变量声明
+        root.walkAtRules(rule => {
+            if (rule.variable) {
+                rule.remove();
             }
         });
     };
