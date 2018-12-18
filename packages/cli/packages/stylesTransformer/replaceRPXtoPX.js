badd4f80a7e93d85498f2aa23d32851da0249b06
blue.yang <blue.yang@qunar.com>
2018-12-18 15:25:08 +0800

    处理浮点数转换

--- packages/cli/packages/stylesTransformer/replaceRPXtoPX.js
+++ packages/cli/packages/stylesTransformer/replaceRPXtoPX.js
@@ -3,7 +3,7 @@ const R = require('ramda');
 const replaceRPXtoPX = declaration => {
     if (declaration.type === 'comment') return; //不解析注释节点
     declaration.value = R.replace(
-        /([-\d]+)(r?px)/g,
+        /([-\d]+)(\.\d+)?(r?px)/g,
         (match, numberStr, unit) => {
             const number = Number(numberStr.trim());
             if (unit === 'rpx') {
