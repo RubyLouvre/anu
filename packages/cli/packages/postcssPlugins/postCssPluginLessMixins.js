85d79d130f658627b2ea2fa3379c707c61a3803b
shaoyudong <shaoyudong33@gmail.com>
2018-12-18 20:48:43 +0800

    fix

--- packages/cli/packages/postcssPlugins/postCssPluginLessMixins.js
+++ packages/cli/packages/postcssPlugins/postCssPluginLessMixins.js
@@ -24,21 +24,25 @@ const postCssPluginLessMixins = postCss.plugin('postCssPluginLessMixins', () => 
                 const mixinName = atrule.raws.identifier + atrule.name;
                 const nodes = findMixins(atrule.parent, mixinName);
                 for (var i = 0, length = nodes.length; i < length; i++) {
-                    const clonedNode = nodes[i].clone({mixin: false});
+                    // 遍历找出的mixin定义节点，替换当前mixin，并传入important参数
+                    const clonedNode = nodes[i].clone({mixin: false, important: atrule.important});
                     atrule.cloneAfter(clonedNode);
                 }
             }
+            
         });
         root.walk((node) => {
+            // 删除mixin调用语句
             if (node.mixin) {
                 node.remove();
             }
+            // 删除带括号的mixin定义语句
             if (node.selector && node.selector.match(reg)) {
                 node.remove();
             }
             
         });
-    }
+    };
 });
 
 module.exports = postCssPluginLessMixins;
