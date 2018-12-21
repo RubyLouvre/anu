6cc29735ae9a9a65d2bb31c9e857e5f7fcad5bd8
RubyLouvre <1669866773@qq.com>
2018-12-21 19:55:04 +0800

    实现useCallback, useMeno

--- packages/fiber/dispatcher.js
+++ packages/fiber/dispatcher.js
@@ -2,87 +2,63 @@ import { Renderer } from 'react-core/createRenderer';
 import { HOOK } from './effectTag';
 function setter(cursor, getter, value) {
     var state = {};
-    if (getter){
-        value = getter(cursor, value);
-    }
-    state[cursor] = value;
-    Renderer.updateComponent(this, state);
+    state[cursor] = getter(cursor, value);
+    Renderer.updateComponent(this.stateNode, state);
 }
 var hookCursor = 0;
 export function resetCursor() {
     hookCursor = 0;
 }
-function useReducer(reducer, initValue, initAction){
-    let instance = Renderer.currentOwner;
-    let cursor = hookCursor;//决定是处理第几个useState
-    let fiber = instance._reactInternalFiber;
-    let pendings = fiber.updateQueue.pendingStates;
-    let getter = reducer ? function (index, action){
-        //reducer需要传入两个值
-        return reducer(pendings[0][index], action || {type: Math.random} );
-    }: null;
-    let dispatch = setter.bind(instance, cursor, getter);
-    hookCursor++;
-    if (fiber.hasMounted) {
-        var newState = {};
-        pendings.unshift(newState);
-        Object.assign.apply(null, pendings);
-        pendings.length = 1;
-        return [newState[cursor], dispatch];
-    }
-    let state = {};
-    state[cursor] = initAction ? reducer(initValue, initAction): initValue;
-    pendings.push(state);
-    return [ state[cursor], dispatch];
-}
+
 export var dispatcher = {
-    /* useState(initValue) {
-        let instance = Renderer.currentOwner;
+    useContext(contextType) {//这个实现并不正确
+        return new contextType.Provider().emitter.get();
+    },
+    useReducer(reducer, initValue, initAction){//ok
         let cursor = hookCursor;//决定是处理第几个useState
-        let fn = setter.bind(instance, cursor, null, null);
-        let fiber = instance._reactInternalFiber;
+        let fiber = getCurrentFiber();
         let pendings = fiber.updateQueue.pendingStates;
+        let getter = reducer ? function (index, action){
+            //reducer需要传入两个值
+            return reducer(pendings[0][index], action || {type: Math.random} );
+        }:  function (index, value){
+            var oldValue = pendings[0][index];
+            return typeof value == 'function' ? value(oldValue) : value;
+        };
+        let dispatch = setter.bind(fiber, cursor, getter);
         hookCursor++;
         if (fiber.hasMounted) {
             var newState = {};
             pendings.unshift(newState);
             Object.assign.apply(null, pendings);
             pendings.length = 1;
-            return [newState[cursor], fn];
+            return [newState[cursor], dispatch];
         }
         let state = {};
-        state[cursor] = initValue;
+        state[cursor] = initAction ? reducer(initValue, initAction): initValue;
         pendings.push(state);
-        return [initValue, fn];
+        return [ state[cursor], dispatch];
     },
-    */
-    useContext(contextType) {
-        return new contextType.Provider().emitter.get();
-    },
-    useReducer: useReducer,
-    useCallback(callback, args) {
-        let instance = Renderer.currentOwner;
-        let fiber = instance._reactInternalFiber;
-        let key = hookCursor +'Cb', fn;
+    useCallbackOrMeno(callback, inputs, isMeno) {//ok
+        var nextInputs = Array.isArray(inputs) ? inputs : [callback];
+        let fiber = getCurrentFiber();
+        let key = hookCursor +'CM';
         let updateQueue = fiber.updateQueue;
         hookCursor++;
-        if (fiber.hasMounted) {
-            fn = updateQueue[key];
-        } else {
-            if (Array.isArray(args) && args.length){
-                fn = callback.bind(null, args);
-            } else {
-                fn = callback;
+        let prevState = updateQueue[key];
+        if (prevState) {
+            var prevInputs = prevState[1];
+            if (areHookInputsEqual(nextInputs, prevInputs)){
+                return prevState[0];
             }
-            updateQueue[key] = fn;
         }
-        Renderer.updateComponent(instance, null, fn);
-        return [fn, fiber.updateQueue.pendingCbs];
+        var value = isMeno ? callback(): callback;
+        updateQueue[key] = [value, nextInputs];
+        return value;
     },
-    useRef(initValue){
-        let instance = Renderer.currentOwner;
+    useRef(initValue){//ok
         let key = hookCursor +'Ref';
-        let fiber = instance._reactInternalFiber;
+        let fiber = getCurrentFiber();
         let updateQueue = fiber.updateQueue;
         hookCursor++;
         if (fiber.hasMounted) {
@@ -91,9 +67,8 @@ export var dispatcher = {
         var ref = updateQueue[key] = {current: initValue};
         return ref;
     },
-    useEffect(callback) {
-        let instance = Renderer.currentOwner;
-        let fiber = instance._reactInternalFiber;
+    useEffect(callback) {//ok
+        let fiber = getCurrentFiber();
         if (fiber.effectTag % HOOK) {
             fiber.effectTag *= HOOK;
         }
@@ -101,3 +76,17 @@ export var dispatcher = {
     }
 };
 //https://reactjs.org/docs/hooks-reference.html
+function getCurrentFiber(){
+    let instance = Renderer.currentOwner;
+    return instance._reactInternalFiber;
+}
+
+function areHookInputsEqual(arr1, arr2) {
+    for (var i = 0; i < arr1.length; i++) {
+        if (Object.is(arr1[i], arr2[i]) ) {
+            continue;
+        }
+        return false;
+    }
+    return true;
+}
