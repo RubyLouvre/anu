bfa4c2be4405cf68c87df9b00e1fd36c75e5d270
tianqi.tian <tianqi.tian@quanr.com>
2018-12-27 14:57:02 +0800

    fix: remove files

--- packages/cli/templates/pdd/babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(api) {
-    api.cache(true);
-    return {
-        'presets':['@babel/preset-env'],
-        'plugins':['@babel/plugin-transform-modules-commonjs'],
-        'babelrcRoots': ['.', 'node_modules']
-    };
-};
