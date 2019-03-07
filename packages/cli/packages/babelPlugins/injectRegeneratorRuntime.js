a7db74d22cbb695c1e6bdd1827d5e2ab1dc198fc
blue.yang <blue.yang@qunar.com>
2019-03-07 11:43:33 +0800

    update async/await parse for babel7

--- packages/cli/packages/babelPlugins/injectRegeneratorRuntime.js
+++ packages/cli/packages/babelPlugins/injectRegeneratorRuntime.js
@@ -2,8 +2,7 @@ let config = require('../config');
 let t = require('@babel/types');
 let hackList = ['wx', 'bu', 'tt', 'quick'];
 
-
-//插入regenerator-runtime/runtime'
+//插入regenerator-runtime/runtime
 let visitor = {
     FunctionDeclaration: {
         exit(astPath) {
@@ -27,51 +26,7 @@ let visitor = {
     }
 };
 
-/**
- *  async function a(){
- *      var t =  await m();
- *  }
- *  
- *  转换成
- * 
- *  let a = async function(){
- *     var t =  await m();
- *  }
- * 
- */
-let visitor2 = {
-    FunctionDeclaration: {
-        enter(astPath){
-            let node = astPath.node;
-            if (!node.async) return;
-            let id = node.id.name;
-            let params = node.params;
-            let body = node.body;
-            astPath.replaceWith(
-                t.variableDeclaration('let', [
-                    t.variableDeclarator(
-                        t.identifier(id),
-                        t.functionExpression(
-                            null,
-                            params,
-                            body,
-                            false,  //generator
-                            true    //async
-                        )
-                    )
-                ])
-            );
-        },
-    }
-};
-
-
 module.exports = [
-    function(){
-        return {
-            visitor: visitor2
-        };
-    },
     require('@babel/plugin-transform-async-to-generator'),
     function(){
         return {
