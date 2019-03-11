b5833ef613fb1ad9e8a29f4bc83165ae591ab435
shaoyudong <shaoyudong33@gmail.com>
2019-03-11 20:38:05 +0800

    fix: 优化cli代码

---
+++ packages/cli/consts/platforms.js
@@ -0,0 +1,27 @@
+module.exports = [
+    {
+        type: 'wx',
+        des: '微信小程序',
+        isDefault: true
+    },
+    {
+        type: 'ali',
+        des: '支付宝小程序'
+    },
+    {
+        type: 'bu',
+        des: '百度智能小程序'
+    },
+    {
+        type: 'tt',
+        des: '头条小程序'
+    },
+    {
+        type: 'quick',
+        des: '快应用'
+    },
+    {
+        type: 'h5',
+        des: 'H5'
+    },
+];
