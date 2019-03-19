8d4c21c832fdbd224120ef078d88cd1c5dfa8795
RubyLouvre <1669866773@qq.com>
2019-03-19 17:24:35 +0800

    如果一个页面的组件被销毁，那么它将不会被小程序重复利用

--- dist/ReactAli.js
+++ dist/ReactAli.js
@@ -1,5 +1,5 @@
 /**
- * 运行于支付宝小程序的React by 司徒正美 Copyright 2019-03-18
+ * 运行于支付宝小程序的React by 司徒正美 Copyright 2019-03-19
  */
 
 var arrayPush = Array.prototype.push;
@@ -679,6 +679,9 @@ function refreshComponent(reactInstances, wx, uuid) {
     for (var i = 0, n = reactInstances.length; i < n; i++) {
         var reactInstance = reactInstances[i];
         if (reactInstance.$$pagePath === pagePath && !reactInstance.wx && reactInstance.instanceUid === uuid) {
+            if (get(reactInstance).disposed) {
+                continue;
+            }
             reactInstance.wx = wx;
             wx.reactInstance = reactInstance;
             updateMiniApp(reactInstance);
