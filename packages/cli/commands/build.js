4dc7cf8af353915f459d70d8348249168685ee34
shaoyudong <shaoyudong33@gmail.com>
2019-03-21 14:48:19 +0800

    fix: 增加错误处理

--- packages/cli/commands/build.js
+++ packages/cli/commands/build.js
@@ -4,10 +4,16 @@ const entry = path.join(process.cwd(), 'source', 'app.js');
 const parser = require('../packages/index')(entry);
 
 module.exports = async function(args){
-    await runBeforeParseTasks(args);
-    await parser.parse();
-    if (args['watch']) {
-        parser.watching();
+    try {
+        await runBeforeParseTasks(args);
+        await parser.parse();
+        if (args['watch']) {
+            parser.watching();
+        }
+    } catch (e) {
+        // eslint-disable-next-line
+        console.log(e);
+        process.exit(1);
     }
 };
 
