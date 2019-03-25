2f0eee78ebdef506af89e237f59e8d5e4d93f1ca
blue.yang <blue.yang@qunar.com>
2019-03-25 15:11:37 +0800

    format wxml

--- packages/cli/packages/wxHelpers/wxml.js
+++ packages/cli/packages/wxHelpers/wxml.js
@@ -2,12 +2,19 @@
 const babel = require('@babel/core');
 const t = require('@babel/types');
 const generate = require('@babel/generator').default;
+const beautify = require('js-beautify');
 const utils = require('../utils');
 const config = require('../config');
 const buildType = config.buildType;
 const attrNameHelper = require(`../${buildType}Helpers/attrName`);
 const attrValueHelper = require(`../${buildType}Helpers/attrValue`);
 const logicHelper = require(`../${buildType}Helpers/logic`);
+
+function beautifyXml(code){
+    return beautify.html(code, {
+        indent: 4
+    });
+}
 
 const quickTextContainer = {
     text: 1,
@@ -39,9 +46,10 @@ function wxml(code, modules) {
     });
     var text = result.code.replace(/&yen;/g, "￥");
     //这里处理中文
-    return text.replace(/\\?(?:\\u)([\da-f]{4})/gi, function(a, b) {
+    text = text.replace(/\\?(?:\\u)([\da-f]{4})/gi, function(a, b) {
         return unescape(`%u${b}`);
     });
+    return beautifyXml(text).trim();
 }
 
 let visitor = {
