b89464b03f73f05c5c8f583d02b5caaca70ec453
shaoyudong <shaoyudong33@gmail.com>
2019-03-28 14:22:38 +0800

    fix: nanachiLoader改为异步处理js

--- packages/nanachi-loader/loaders/nanachiLoader.js
+++ packages/nanachi-loader/loaders/nanachiLoader.js
@@ -6,20 +6,28 @@ const isReact = function(sourcePath){
     return /React\w+\.js$/.test(path.basename(sourcePath));
 };
 
-module.exports = function(code, map, meta) {
+module.exports = async function(code, map, meta) {
     const relativePath = path.relative(path.resolve(this.rootContext, 'source'), this.resourcePath);
+    const callback = this.async();
     try {
         // TODO 抽离路径，纯净plugin
         
         if (isReact(this.resourcePath)) {
-            return [{
+            // return [{
+            //     isDefault: true,
+            //     type: 'js',
+            //     path: relativePath,
+            //     code: code
+            // }];
+            callback(null, [{
                 isDefault: true,
                 type: 'js',
                 path: relativePath,
                 code: code
-            }];
+            }], map, meta);
+            return;
         }
-        const res = babel.transformFileSync(this.resourcePath, {
+        const res = await babel.transformFileAsync(this.resourcePath, {
             configFile: false,
             babelrc: false,
             comments: false,
@@ -70,15 +78,15 @@ module.exports = function(code, map, meta) {
             code: res.code,
             extraModules: res.options.anu.extraModules
         });
+
+        callback(null, res.options.anu.queue, map, meta);
+        return;
     
-        return res.options.anu.queue;
+        // return res.options.anu.queue;
     } catch (e) {
         console.log(e);
-        return [{
-            type: 'error',
-            path: relativePath,
-            code: e
-        }];
+        callback(e, '', map, meta);
+        return;
     }
     
 };
