e15d44a670c0f06f203882b2b256ea36edbc6cfb
shaoyudong <shaoyudong33@gmail.com>
2019-04-03 15:26:58 +0800

    opti: code

--- packages/cli/commands/build.js
+++ packages/cli/commands/build.js
@@ -27,7 +27,7 @@ module.exports = async function(args){
     try {
         
         // await runBeforeParseTasks(args);
-        const { buildType, beta, betaUi } = args;
+        const { buildType, beta, betaUi, watch, compress } = args;
         // const parser = JavascriptParserFactory.create({
         //     platform: 'wx',
         //     filepath: path.resolve(cwd, 'source/app.js')
@@ -43,8 +43,8 @@ module.exports = async function(args){
             platform: buildType,
             beta,
             betaUi,
-            compress: config['compress'],
-            watch: args['watch'] ? true : false,
+            compress,
+            watch,
             plugins: [
                 new CleanWebpackPlugin()     
             ],
