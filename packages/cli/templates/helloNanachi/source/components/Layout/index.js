51c5600abac8de04a31b11ec5197acce28793442
RubyLouvre <1669866773@qq.com>
2019-05-15 11:31:01 +0800

    fix Layout的依赖BUG

--- packages/cli/templates/helloNanachi/source/components/Layout/index.js
+++ packages/cli/templates/helloNanachi/source/components/Layout/index.js
@@ -1,5 +1,5 @@
 import React from '@react';
-import { GlobalTheme } from '@common/GlobalTheme';
+import { GlobalTheme } from '@common/GlobalTheme/index';
 export default function Layout(props) {
     const globalStyle = React.useContext(GlobalTheme);
     console.log('Layout init', globalStyle); //debug
