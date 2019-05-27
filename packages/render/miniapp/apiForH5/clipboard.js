bfb65b9d10e6853ce512d8239f1fbda83fa86764
shaoyudong <shaoyudong33@gmail.com>
2019-05-27 10:40:40 +0800

    fix: commit

---
+++ packages/render/miniapp/apiForH5/clipboard.js
@@ -0,0 +1,52 @@
+import Clipboard from 'clipboard';
+import { handleSuccess, handleFail } from '../shared/utils/handleCallback';
+
+/**
+ * 获取系统剪贴板的内容
+ */
+function getClipboardData() {
+  // 暂时无法实现
+}
+
+/**
+ * 设置系统剪贴板的内容
+ */
+function setClipboardData(options = {}) {
+  return new Promise(function(resolve, reject) {
+    const {
+      data = '',
+      success = () => {},
+      fail = () => {},
+      complete = () => {}
+    } = options;
+    try {
+      const aux = document.createElement('input');
+      aux.setAttribute('data-clipboard-text', data);
+      new Clipboard(aux);
+      aux.click();
+      handleSuccess(
+        {
+          errMsg: 'setClipboardData success',
+          data
+        },
+        success,
+        complete,
+        resolve
+      );
+    } catch (e) {
+      handleFail(
+        {
+          errMsg: e
+        },
+        fail,
+        complete,
+        reject
+      );
+    }
+  });
+}
+
+export default {
+  // getClipboardData,
+  setClipboardData
+};
