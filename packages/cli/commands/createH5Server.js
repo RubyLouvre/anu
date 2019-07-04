ab80dc09bb2398254963027e6b813819419b14dd
shaoyudong <shaoyudong33@gmail.com>
2019-07-04 14:53:11 +0800

    fix: h5 dev server端口号冲突问题

--- packages/cli/commands/createH5Server.js
+++ packages/cli/commands/createH5Server.js
@@ -2,13 +2,17 @@ const webpackDevServer = require('webpack-dev-server');
 const webpackH5Config = require('../config/h5/webpack.config.js');
 let app;
 const PORT = 8080;
+const getPort = require('get-port');
 
-module.exports = function(compiler) {
+module.exports = async function(compiler) {
     if (!app) {
+        const port = await getPort({
+            port: PORT
+        });
         app = new webpackDevServer(compiler, {
             publicPath: webpackH5Config.output.publicPath,
             host: '0.0.0.0',
-            port: PORT,
+            port,
             historyApiFallback: {
                 rewrites: [{
                     from: /.*/g,
@@ -24,6 +28,6 @@ module.exports = function(compiler) {
                 poll: 500
             }
         });
-        app.listen(PORT);
+        app.listen(port);
     }
 };
