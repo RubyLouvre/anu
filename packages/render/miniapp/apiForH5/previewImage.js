93f5e322763a65a015b5c8f2a612028eff14cc91
qitmac001060 <qitmac001060@qitmac001060deMacBook-Pro.local>
2019-08-11 14:19:54 +0800

    fix: 修改previewImage

--- packages/render/miniapp/apiForH5/previewImage.js
+++ packages/render/miniapp/apiForH5/previewImage.js
@@ -1,12 +1,7 @@
 // TODO 用React Portals 可以更好的实现
 import React from '@react';
 import { Component } from 'react-core/Component';
-import { DOMRenderer } from '../../dom/DOMRenderer';
-// import Lightbox from 'react-images';
-import { handleSuccess, handleFail } from '../utils';
-
-// let that = null;
-// let container = null;
+import { handleSuccess } from '../utils';
 
 class PreviewImage extends Component {
     constructor(props) {
@@ -20,7 +15,6 @@ class PreviewImage extends Component {
         this.close = this.close.bind(this);
         this.gotoPrevious = this.gotoPrevious.bind(this);
         this.gotoNext = this.gotoNext.bind(this);
-        this.gotoImage = this.gotoImage.bind(this);
     }
     componentDidMount() {
         handleSuccess(
@@ -35,58 +29,57 @@ class PreviewImage extends Component {
 
     componentWillUnmount() {
         React.api.previewImageSingleton = null;
-        document.removeChild(this.container);
+        var el = this.container;
+        if (el && el.parentNode){
+            el.parentNode.removeChild(el);
+        }
     }
 
-    gotoPrevious() {
+    gotoPrevious() {  // 上一张图片
+        var urls = this.props.urls;
+        var index =  urls.indexOf(this.state.current)-1;
+        if (index < 0){
+            index =  urls.length-1;
+        }
         this.setState({
-            current: this.state.current - 1
+            current: urls[index]
         });
     }
 
-    gotoNext() {
+    gotoNext() {     // 下一张图片
+        var urls = this.props.urls;
+        var index =  urls.indexOf(this.state.current)+1;
+        if (index === urls.length){
+            index =  0;
+        }
         this.setState({
-            current: this.state.current + 1
-        });
-    }
-
-    gotoImage(index) {
-        this.setState({
-            current: index
+            current: urls[index]
         });
     }
 
     close() {
-        this.setState({
-            visible: false
-        });
+        this.componentWillUnmount();
     }
 
     render() {
-        // const { visible, urls, current } = this.state;
-
-        // this.container.style = visible
-        //     ? 'width: 100%;height: 100%;position: fixed;'
-        //     : 'none';
-
         return (
-            <div>
-                {
-                    // React.api.createModal(<p>q;</p>)
-                }
+            <div className='showImg2019'>
                 <image src={this.state.current} />
-                {/* <Lightbox
-                    images={urls.map(src => {
-                        return {
-                        src
-                        };
-                    })}
-                    isOpen={visible}
-                    currentImage={current}
-                    onClickPrev={this.gotoPrevious}
-                    onClickNext={this.gotoNext}
-                    onClose={this.close}
-                    /> */}
+                <style ref={(node) => {
+                    Object(node).textContent = `
+                    .showImg2019{
+                        display: flex;
+                        flex-direction: column;
+                        align-items: center;
+                        justify-content: center;
+                        position: fixed;
+                        width:100%;
+                    }
+                    .showImg2019 img{
+                        width:100%;
+                    }`;
+                }}>
+                </style>
             </div>
         );
     }
@@ -107,29 +100,69 @@ function previewImage(options = {}) {
             complete = () => {}
         } = options;
         var instance = React.api.previewImageSingleton;
-        if (!instance){
-            var internalModal = document.getElementsByClassName('__internal__Modal__')[0];
+        var el = document.querySelector('#h5-api-previewImage');  // 获取页面中的div节点
+        if (!el){  // 判断当前是否创建了元素节点，若没有创建。则进行创建
+            var id = 'h5-api-previewImage';
+            var imgModal = document.querySelector('.__internal__Page-container');
             var container = document.createElement('div');
-            internalModal.appendChild(container);
-            // document.body.appendChild(container);
+            container.id = id;
+            container.style.position = 'fixed';// fixed absolute
+            container.style.top = 0;
+            container.style.width = '100%';
+            container.style.height = '100%';
+            container.style.backgroundColor = 'rgba(0,0,0,0.7)';
+            container.style.display = 'flex';
+            container.style.justifyContent = 'center';
+            container.style.alignItems = 'center';
+            // container.style.pointerEvents = 'auto';
+            imgModal.appendChild(container);
+    
+            var startX, startTimeX = 0;
+            container.ontouchstart = function(e){
+                startX = e.touches[0].pageX;
+                startTimeX = e.timeStamp;
+            };
+            container.ontouchend = function(e){
+                var endX = e.changedTouches[0].pageX;
+                var endStartTime = e.timeStamp;
+                if (!instance || !e.changedTouches[0]){
+                    return;
+                }
+                //  console.log(endX-startX, 'Math.abs(endX-startX)<10', Math.abs(endX-startX),';;;;;;;;;;', endStartTime - startTimeX);
+                if (endStartTime - startTimeX <= 200 || Math.abs(endX-startX)<10){  //  用这个&& 长按不关闭
+                    //点击事件
+                    instance.close();
+                    startTimeX = 0;
+                    return;
+                }
+                if (startX != null){
+                    if (endX-startX > 0){
+                        instance.gotoPrevious();
+                        startX = null;
+                    } else {
+                        instance.gotoNext();
+                        startX = null;
+                    }
+                }
+            };
             React.render(
                 <PreviewImage
                     success={success}
                     fail={fail}
+                    urls={urls}
                     container={container}
                     complete={complete}
                     resolve={resolve}
                     reject={reject}
-                    ref={(refs) => {
+                    ref={(refs)=>{
                         if (refs){
                             instance = React.api.previewImageSingleton = refs;
                         }
                     }}
                 />,
-                container, function() {
+                container,function(){
                     instance.setState({
                         visible: true,
-                        urls,
                         current
                     });
                 }
@@ -137,7 +170,6 @@ function previewImage(options = {}) {
         } else {
             instance.setState({
                 visible: true,
-                urls,
                 current
             });
         }
