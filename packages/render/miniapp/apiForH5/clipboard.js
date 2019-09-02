6445c3542e0837e5b656ff871a476b1709816ea0
RubyLouvre <1669866773@qq.com>
2019-09-02 17:20:36 +0800

    添加一个只支持chrome浏览器的剪贴版方案

--- packages/render/miniapp/apiForH5/clipboard.js
+++ packages/render/miniapp/apiForH5/clipboard.js
@@ -4,29 +4,14 @@ import { handleSuccess, handleFail } from "../utils";
  * 设置系统剪贴板的内容
  * https://w3c.github.io/clipboard-apis/#dom-clipboard-writetext
  */
-function setClipboardData({ data, success, fail, complete }) {
+function coolieMethod(input, success, fail, complete, method, msg) {
+    try{
     return navigator.clipboard
-        .writeText(data)
-        .then(function() {
-            var ok = {
-                errMsg: "setClipboardData success",
-                data
-            };
-            handleSuccess(ok, success, complete);
-        })
-        .catch(function(e) {
-            var ng = { data: null, errMsg: e };
-            handleFail(ng, fail, complete);
-        });
-}
-
-function getClipboardData({ success, fail, complete }) {
-   return navigator.clipboard
-        .readText()
+        [method](input)
         .then(function(data) {
             var ok = {
-                errMsg: "getClipboardData success",
-                data
+                errMsg: msg,
+                data: data
             };
             handleSuccess(ok, success, complete);
         })
@@ -34,7 +19,21 @@ function getClipboardData({ success, fail, complete }) {
             var ng = { data: null, errMsg: e };
             handleFail(ng, fail, complete);
         });
+    }catch(e){
+      return  Promise.reject({
+        data: null, errMsg: e
+      }).catch(function(reason){
+         handleFail(reason, fail, complete);
+      })
+    }
 }
+function setClipboardData({data, success, fail, complete }){
+    return coolieMethod(data, success, fail, complete, "writeText", 'setClipboardData:ok' )
+}
+function getClipboardData({success, fail, complete }){
+    return coolieMethod(null, success, fail, complete, "readText", 'getClipboardData:ok' )
+}
+
 
 export default {
     getClipboardData,
