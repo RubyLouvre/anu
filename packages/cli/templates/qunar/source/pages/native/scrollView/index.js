76ca90ebcad67ef84226d583c680f3612a01f900
RubyLouvre <1669866773@gmail.com>
2019-12-18 17:27:25 +0800

    更新模板

--- packages/cli/templates/qunar/source/pages/native/scrollView/index.js
+++ packages/cli/templates/qunar/source/pages/native/scrollView/index.js
@@ -1,13 +1,13 @@
 import React from '@react';
 import './index.scss';
-
+import ScrollItemDiv from '@nativeComponents/ScrollItemDiv/index'
 class P extends React.Component {
     constructor() {
         super();
-        this.order = ['red', 'yellow', 'blue', 'green', 'red'];
+        this.order = ['red', 'yellow', 'blue', 'green'];
         this.state = {
-            toView: 'red',
-            scrollTop: 100
+            itemId: 'red',
+            scrollTop: 0
         };
     }
 
@@ -26,19 +26,29 @@ class P extends React.Component {
         console.log(e);
     }
 
-    tap() {
-        for (var i = 0; i < this.order.length; ++i) {
-            if (this.order[i] === this.state.toView) {
-                this.setState({
-                    toView: this.order[i + 1]
-                });
-                break;
-            }
+    scrollById(itemId) {
+        console.log("scrollById", itemId)
+      
+        if( typeof this.wx.scrollTo === 'function'){
+            var itemIndex = this.order.indexOf(itemId)
+            this.wx.scrollTo( {
+                index: itemIndex,
+                smooth: true
+            })
         }
+        
+    
+        this.setState({
+             itemId: itemId
+        });
+                 
+        
     }
 
-    tapMove() {
+    scrollByPx() {
+        console.log("scrollByPx")
         var _self = this;
+       
         this.setState({
             scrollTop: _self.state.scrollTop + 10
         });
@@ -46,33 +56,46 @@ class P extends React.Component {
 
     render() {
         return (
-            <div>
-                <div class="section">
-                    <div class="section__title">vertical scroll</div>
+            <div class="page-body column-layout">
+                <div class="page-section column-layout">
+                    <div class="page-section-title">vertical scroll</div>
                     <scroll-view
                         scroll-y
                         style="height: 200px"
+
+                     
+                        scroll-into-view={this.state.itemIndex}
+
                         onScrollToUpper={this.upper}
                         onScrollToLower={this.lower}
                         onScroll={this.scroll}
-                        scroll-into-div={this.state.todiv}
+                      
                         scroll-top={this.state.scrollTop}
                     >
-                        <list-item type="green" id="green"  class="scroll-view-item bc_green" />
-                        <list-item type="red"  class="scroll-view-item bc_red" />
-                        <list-item type="yellow"  class="scroll-view-item bc_yellow" />
-                        <list-item type="blue"  class="scroll-view-item bc_blue" />
+                        <list-item type="green" id="green"  class="scroll-view-item bc_green" >
+                           <ScrollItemDiv name="green" index={0} />
+                        </list-item>
+                        <list-item type="red"   id="red"  class="scroll-view-item bc_red" >
+                           <ScrollItemDiv name="red" index={1} />
+                        </list-item> 
+                        <list-item type="yellow"  id="yellow" class="scroll-view-item bc_yellow" >
+                           <ScrollItemDiv name="yellow" index={2} />
+                        </list-item>
+                        <list-item type="blue"  id="blue" class="scroll-view-item bc_blue" >
+                           <ScrollItemDiv name="blue" index={3} />
+                        </list-item>
                     </scroll-view>
 
-                    <div class="btn-area">
-                        <button size="mini" onTap={this.tap}>
-                            click me to scroll into div{' '}
-                        </button>
-                        <button size="mini" onTap={this.tapMove}>
+                    <div class="anu-block">
+                        <div class="fake-button" size="mini" onTap={this.scrollById.bind(this, 'green')}>
+                            scroll into green div
+                        </div>
+                        <div class="fake-button" size="mini" onTap={this.scrollByPx.bind(this)}>
                             click me to scroll
-                        </button>
+                        </div>
                     </div>
                 </div>
+                {/*
                 <div class="section section_gap">
                     <div class="section__title">horizontal scroll</div>
                     <scroll-view
@@ -86,6 +109,7 @@ class P extends React.Component {
                         <list-item type="blue" id="blue" class="scroll-view-item_H bc_blue" />
                     </scroll-view>
                 </div>
+                */}
             </div>
         );
     }
