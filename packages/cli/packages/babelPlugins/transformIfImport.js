3c3ac25eee6e539b687d3b67aafa4500d5a71e2c
aweleey <ileeyi@qq.com>
2020-09-04 15:53:17 +0800

    opti: 按需引入 import , 不需要的文件不会出现在 dist 目录

--- packages/cli/packages/babelPlugins/transformIfImport.js
+++ packages/cli/packages/babelPlugins/transformIfImport.js
@@ -1,31 +1,40 @@
 "use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const t = __importStar(require("@babel/types"));
 const config_1 = __importDefault(require("../../config/config"));
-const envReg = /\s*if\s+process\.env\.ANU_ENV\s*={2,3}\s*'([\w|]*)';?/;
+const envReg = /\s*if\s+process\.env\.ANU_ENV\s*={2,3}\s*\[(.*)\];?/;
 let visitor = {
     Program: {
         enter(astPath) {
             const nodes = astPath.node.body;
-            astPath.node.body = nodes.filter((node) => {
+            astPath.node.body = nodes.filter(node => {
                 const leadingComments = node.leadingComments;
                 if (node.type === 'ImportDeclaration' && leadingComments) {
                     for (let i = 0; i < leadingComments.length; i++) {
-                        let commentValue = leadingComments[i].value;
+                        const { type, value: commentValue } = leadingComments[i];
                         const match = commentValue.match(envReg);
-                        if (leadingComments[i].type === 'CommentLine'
-                            && match) {
-                            const targetEnvs = match[1] && match[1].split('|');
+                        if (type === 'CommentLine' && match) {
+                            const targetEnvs = match[1];
                             if (targetEnvs && !targetEnvs.includes(config_1.default.buildType)) {
                                 return false;
                             }
+                            t.removeComments(node);
                         }
                     }
                 }
                 return true;
             });
+            console.log();
         }
     }
 };
--- src/cli/packages/babelPlugins/transformIfImport.ts
+++ src/cli/packages/babelPlugins/transformIfImport.ts
@@ -1,42 +1,53 @@
-//条件import
-/**
- *  // if process.env.ANU_ENV === [wx|ali|bu|quick]
- *  import ...
- */
 import * as t from '@babel/types';
 import { NodePath, PluginObj } from '@babel/core';
 import config from '../../config/config';
-const envReg = /\s*if\s+process\.env\.ANU_ENV\s*={2,3}\s*'([\w|]*)';?/;
+const envReg = /\s*if\s+process\.env\.ANU_ENV\s*={2,3}\s*\[(.*)\];?/;
+/**
+ * 条件import
+ *
+ * ``` js
+ * // if process.env.ANU_ENV === [wx|ali|bu|quick]
+ * import xxx from 'xxx'
+ * ```
+ */
 let visitor = {
     Program: {
         // 必须enter时删除，在其他插件解析前就将无用的import语句删除
         enter(astPath: NodePath<t.Program>) {
             const nodes = astPath.node.body;
             // 此处不能使用visitor访问import节点，因为删除节点后可能会改变下一个import语句的leadingComments，必须使用遍历节点的方式实现按需打包
-            astPath.node.body = nodes.filter((node) => {
+            astPath.node.body = nodes.filter(node => {
                 const leadingComments = node.leadingComments;
+                /**
+                 * 使用了 import 语句,并且import上方有注释
+                 * leadingComments: import 上方可能有多条注释 
+                 */
                 if (node.type === 'ImportDeclaration' && leadingComments) {
-                    for (let i = 0; i < leadingComments.length; i++){
-                        let commentValue = leadingComments[i].value;
+                    for (let i = 0; i < leadingComments.length; i++) {
+                        const { type, value: commentValue } = leadingComments[i];
                         const match = commentValue.match(envReg);
-                        if (
-                            leadingComments[i].type === 'CommentLine' //单行注释
-                            && match            //满足if语句
-                        ) { 
-                            const targetEnvs: any = match[1] && match[1].split('|');
+                        /**
+                         * CommentLine 单行注释
+                         * march: 匹配注释 if process.env.ANU_ENV === [wx|ali|bu|quick]
+                         */
+                        if (type === 'CommentLine' && match) {
+                            const targetEnvs: any = match[1];
                             //移除无法匹配ANU_ENV的import语句
                             if (targetEnvs && !targetEnvs.includes(config.buildType)) {
                                 return false;
                             }
+                            // 删掉对应的注释
+                            t.removeComments(node);
                         }
                     }
                 }
                 return true;
             });
+            console.log();
         }
     }
 };
-module.exports = function(): PluginObj{
+module.exports = function (): PluginObj {
     return {
         visitor: visitor
     };
